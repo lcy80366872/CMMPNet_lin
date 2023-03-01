7383a54059a7fe37250234563f92645e4cc9d931
lcy80366872 <106853511+lcy80366872@users.noreply.github.com>
2023-03-01 11:53:31 +0800

    Update main.py

--- main.py
+++ main.py
@@ -82,7 +82,7 @@ if __name__ == "__main__":
     parser.add_argument('--val_size', type=float, default=0.1)
     parser.add_argument('--use_gpu',  type=bool, default=True)
     parser.add_argument('--gpu_ids',  type=str, default='0')
-    parser.add_argument('--workers',  type=int, default=4)
+    parser.add_argument('--workers',  type=int, default=1)
     parser.add_argument('--epochs',  type=int, default=1)
     parser.add_argument('--random_seed', type=int, default=12345)
     parser.add_argument('--dataset', type=str, default='BJRoad')
