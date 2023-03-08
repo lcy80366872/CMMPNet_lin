b8ab8048611a7c019f33724adc3d718bcb3be441
lcy80366872 <106853511+lcy80366872@users.noreply.github.com>
2023-03-08 19:11:56 +0800

    Update loss.py

--- loss.py
+++ loss.py
@@ -4,6 +4,64 @@ import cv2
 import numpy as np
 
 
+class SegmentationLosses(object):
+    def __init__(self, weight=None, size_average=True, batch_average=True, ignore_index=255, cuda=False):
+        self.ignore_index = ignore_index
+        self.weight = weight
+        self.size_average = size_average
+        self.batch_average = batch_average
+        self.cuda = cuda
+
+    def build_loss(self, mode='ce'):
+        """Choices: ['ce' or 'focal']"""
+        if mode == 'ce':
+            return self.CrossEntropyLoss
+        elif mode == 'focal':
+            return self.FocalLoss
+        elif mode =='con_ce':
+            return self.ConLoss
+        else:
+            raise NotImplementedError
+
+    def CrossEntropyLoss(self, logit, target):
+        n, c, h, w = logit.size()
+        criterion = nn.CrossEntropyLoss(weight=self.weight, #ignore_index=self.ignore_index,
+                                        size_average=self.size_average)
+        if self.cuda:
+            criterion = criterion.cuda()
+
+        loss = criterion(logit, target.long())
+
+        if self.batch_average:
+            loss /= n
+
+        return loss
+
+    def ConLoss(self, logit, target):
+        # loss = torch.mean(torch.sum(-target * torch.log(F.softmax(logit, dim=1)), dim=1))
+        # loss = torch.mean(torch.sum(-target * nn.LogSoftmax()(logit), dim=1))
+        loss = nn.BCEWithLogitsLoss()(logit, target)
+        # loss = nn.BCELoss()(logit, target)
+        return loss
+
+    def FocalLoss(self, logit, target, gamma=2, alpha=0.25):
+        n, c, h, w = logit.size()
+        criterion = nn.CrossEntropyLoss(weight=self.weight, #ignore_index=self.ignore_index,
+                                        size_average=self.size_average)
+        if self.cuda:
+            criterion = criterion.cuda()
+
+        logpt = -criterion(logit, target.long())
+        pt = torch.exp(logpt)
+        if alpha is not None:
+            logpt *= alpha
+        loss = -((1 - pt) ** gamma) * logpt
+
+        if self.batch_average:
+            loss /= n
+
+        return loss
+
 class dice_bce_loss(nn.Module):
     def __init__(self, batch=True):
         super(dice_bce_loss, self).__init__()
