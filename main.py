c906bca353a6d12e3fe730eb702a822275b8f355
lcy80366872 <106853511+lcy80366872@users.noreply.github.com>
2023-03-08 21:57:39 +0800

    Update main.py

--- main.py
+++ main.py
@@ -51,8 +51,8 @@ def predicting_road(img):
 
 def train_val_test(args):
     net = get_model(args.model)
-    with torch.no_grad():  # 必须有
-        summary(net.to('cuda'), input_size=(4, 512, 512), batch_size=1)
+#     with torch.no_grad():  # 必须有
+#         summary(net.to('cuda'), input_size=(4, 512, 512), batch_size=1)
     print(net)
     
     optimizer = torch.optim.Adam(params=net.parameters(), lr=args.lr)
