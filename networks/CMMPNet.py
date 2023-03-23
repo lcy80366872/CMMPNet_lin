7f70aed7c2d1aa48685370d50413d9de6e403cc1
lcy80366872 <106853511+lcy80366872@users.noreply.github.com>
2023-03-23 14:13:32 +0800

    Update CMMPNet.py

--- networks/CMMPNet.py
+++ networks/CMMPNet.py
@@ -1,3 +1,4 @@
+
 import torch
 from torchvision import models
 from .basic_blocks import *
@@ -5,7 +6,7 @@ import math
 import torch.nn.functional as F
 from networks.MPM import SPHead
 up_kwargs = {'mode': 'bilinear', 'align_corners': True}
-
+#spp只到拼接，后面的fc层步骤不算在spp内
 class SPPLayer(torch.nn.Module):
     def __init__(self, block_size=[1,2,4], pool_type='max_pool'):
         super(SPPLayer, self).__init__()
@@ -25,10 +26,13 @@ class SPPLayer(torch.nn.Module):
     def forward(self, x):
         num = x.size(0)
         for i in range(len(self.block_size)):
+            #view：返回一个有相同数据但大小不同的tensor。 返回的tensor必须有与原tensor相同的数据和相同数目的元素，但可以有不同的大小，大小可以自己选
+            #将spp每个分支展成一维向量，再拼接
             tensor = self.spp[i](x).view(num, -1)
             if (i == 0):
                 x_flatten = tensor.view(num, -1)
             else:
+                #在第1维度拼接，也就是横向拼接成长条
                 x_flatten = torch.cat((x_flatten, tensor.view(num, -1)), 1)
 
         return x_flatten
@@ -38,13 +42,13 @@ class SPPLayer(torch.nn.Module):
 class DEM(torch.nn.Module): # Dual Enhancement Module
     def __init__(self, channel, block_size=[1,2,4]):
         super(DEM, self).__init__()
-
-        self.rgb_local_message = self.local_message_prepare(channel, 1, 1, 0)
+        #这里相当于1*1卷积了 ，padding=0
+        self.rgb_local_message = self.local_message_prepare(channel, 1, 1, 0)     #文中的L
         self.add_local_message = self.local_message_prepare(channel, 1, 1, 0)
         
         self.rgb_spp = SPPLayer(block_size=block_size)
         self.add_spp = SPPLayer(block_size=block_size)
-        self.rgb_global_message = self.global_message_prepare(block_size, channel)
+        self.rgb_global_message = self.global_message_prepare(block_size, channel) #文中的G
         self.add_global_message = self.global_message_prepare(block_size, channel)
         
         self.rgb_local_gate  = self.gate_build(channel*2, channel, 1, 1, 0)
@@ -60,11 +64,11 @@ class DEM(torch.nn.Module): # Dual Enhancement Module
             nn.BatchNorm2d(dim)
         )
 
+    # 下面做的是spp得到拼接向量之后的步骤FC+relu
     def global_message_prepare(self, block_size, dim):
         num_block = 0
         for i in block_size:
             num_block += i*i
-        
         return  nn.Sequential(
             nn.Linear(num_block*dim, dim),
             nn.ReLU()
@@ -79,10 +83,12 @@ class DEM(torch.nn.Module): # Dual Enhancement Module
     def forward(self, rgb_info, add_info):
         rgb_local_info = self.rgb_local_message(rgb_info)
         add_local_info = self.add_local_message(add_info)
-        
+        #就是在指定的位置插入一个维度，有两个参数，input是输入的tensor,dim是要插到的维度
+        #https://blog.csdn.net/ljwwjl/article/details/115342632
+        #SPP+FC+RELU+扩展成（N*c*1*1）+复制成N*c*h*w
         rgb_global_info = torch.unsqueeze(torch.unsqueeze(self.rgb_global_message(self.rgb_spp(rgb_local_info)),-1),-1).expand(rgb_local_info.size())
         add_global_info = torch.unsqueeze(torch.unsqueeze(self.add_global_message(self.add_spp(add_local_info)),-1),-1).expand(add_local_info.size())
-                
+        #add_local_gate的输出大小也为N*C*H*W
         rgb_info = rgb_info + add_local_info * self.add_local_gate(torch.cat((add_local_info, add_global_info), 1)) + add_global_info * self.add_global_gate(torch.cat((add_local_info, add_global_info), 1))
         add_info = add_info + rgb_local_info * self.rgb_local_gate(torch.cat((rgb_local_info, rgb_global_info), 1)) + rgb_global_info * self.rgb_global_gate(torch.cat((rgb_local_info, rgb_global_info), 1))
 
@@ -95,7 +101,7 @@ class DinkNet34_CMMPNet(nn.Module):
         filters = [64, 128, 256, 512]
         self.net_name = "CMMPnet"       
         self.block_size = [int(s) for s in block_size.split(',')]
-        
+
         # img
         resnet = models.resnet34(pretrained=True)
         self.firstconv1 = resnet.conv1
@@ -107,9 +113,9 @@ class DinkNet34_CMMPNet(nn.Module):
         self.encoder2 = resnet.layer2
         self.encoder3 = resnet.layer3
         self.encoder4 = resnet.layer4
-        
-        self.head = SPHead(filters[3],filters[3], nn.BatchNorm2d, up_kwargs)
+
         self.dblock = DBlock(filters[3])
+        self.head = SPHead(filters[3], filters[3], nn.BatchNorm2d, up_kwargs)
 
         self.decoder4 = DecoderBlock(filters[3], filters[2])
         self.decoder3 = DecoderBlock(filters[2], filters[1])
@@ -133,9 +139,9 @@ class DinkNet34_CMMPNet(nn.Module):
         self.encoder2_add = resnet1.layer2
         self.encoder3_add = resnet1.layer3
         self.encoder4_add = resnet1.layer4
-        
-        self.head = SPHead(filters[3],filters[3], nn.BatchNorm2d, up_kwargs)
-        self.dblock_add = DBlock(filters[3])
+
+        # self.dblock_add = DBlock(filters[3])
+        # self.head = SPHead(filters[3], filters[3], nn.BatchNorm2d, up_kwargs)
 
         self.decoder4_add = DecoderBlock(filters[3], filters[2])
         self.decoder3_add = DecoderBlock(filters[2], filters[1])
@@ -154,10 +160,10 @@ class DinkNet34_CMMPNet(nn.Module):
         self.dem_e3 = DEM(filters[2], self.block_size)
         self.dem_e4 = DEM(filters[3], self.block_size)
 
-        self.dem_d4 = DEM(filters[2], self.block_size)        
+        self.dem_d4 = DEM(filters[2], self.block_size)
         self.dem_d3 = DEM(filters[1], self.block_size)
         self.dem_d2 = DEM(filters[0], self.block_size)
-        self.dem_d1 = DEM(filters[0], self.block_size)    
+        self.dem_d1 = DEM(filters[0], self.block_size)
                     
                     
         self.finalconv = nn.Conv2d(filters[0], 1, 3, padding=1)
@@ -167,12 +173,12 @@ class DinkNet34_CMMPNet(nn.Module):
 
         x   = inputs[:,:3,:,:]    # image
         add = inputs[:,3:,:,:]    # gps_map or lidar_map
-
+        #进入编码-解码结构前有个将原图像做卷积步骤
         x   = self.firstconv1(x)
         add = self.firstconv1_add(add)
         x   = self.firstmaxpool(self.firstrelu(self.firstbn(x)))
         add = self.firstmaxpool_add(self.firstrelu_add(self.firstbn_add(add)))
-
+        #每一层的图像和adding的额外信息例如gps都输入DEM模块，输出增强的图像和adding特征信息，然后再输入下一层以此循环
         x_e1   = self.encoder1(x)
         add_e1 = self.encoder1_add(add)
         x_e1, add_e1 = self.dem_e1(x_e1, add_e1)
@@ -190,15 +196,14 @@ class DinkNet34_CMMPNet(nn.Module):
         x_e4, add_e4 = self.dem_e4(x_e4, add_e4)
         
         # Center
-        
-#         x_e4  = self.head(x_e4)
-#         add_e4= self.head(add_e4)
+        # x_e4  = self.head(x_e4)
+        # add_e4= self.head(add_e4)
         x_c   = self.dblock(x_e4)
-        add_c = self.dblock_add(add_e4)
-
+        # add_c = self.dblock_add(add_e4)
+        #传递增强信息时还有跳跃连接
         # Decoder
         x_d4   = self.decoder4(x_c) + x_e3
-        add_d4 = self.decoder4_add(add_c) + add_e3
+        add_d4 = self.decoder4_add(add_e4) + add_e3
         x_d4, add_d4 = self.dem_d4(x_d4, add_d4)
         
         x_d3   = self.decoder3(x_d4) + x_e2
