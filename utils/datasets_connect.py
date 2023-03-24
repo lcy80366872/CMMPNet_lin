1e843f944eec3283dd906c6c8e2455eb24ec1ece
lcy80366872 <106853511+lcy80366872@users.noreply.github.com>
2023-03-24 21:57:34 +0800

    Update datasets_connect.py

--- utils/datasets_connect.py
+++ utils/datasets_connect.py
@@ -29,9 +29,7 @@ def prepare_Beijing_dataset(args):
     image_list = [x[:-9] for x in os.listdir(args.mask_dir)      if x.find('mask.png') != -1]
     test_list  = [x[:-9] for x in os.listdir(args.test_mask_dir) if x.find('mask.png') != -1]
     train_list, val_list = train_test_split(image_list, test_size=args.val_size, random_state=args.random_seed)
-#     print('train_list:',train_list)
-#     print('val_list:',val_list)
-#     print('test_list:',test_list)
+    
     train_dataset = ImageGPSDataset(train_list, args.sat_dir,      args.mask_dir,      args.gps_dir,  args.connect_dir1 ,  args.connect_dir2, randomize=True,  down_scale=args.down_scale)
     val_dataset   = ImageGPSDataset(val_list,   args.sat_dir,      args.mask_dir,      args.gps_dir,  args.connect_dir1 ,  args.connect_dir2,   randomize=False, down_scale=args.down_scale)
     test_dataset  = ImageGPSDataset(test_list,  args.test_sat_dir, args.test_mask_dir, args.test_gps_dir,  args.test_connect_dir1,args.test_connect_dir2,randomize=False, down_scale=args.down_scale)
@@ -65,6 +63,34 @@ def prepare_TLCGIS_dataset(args):
     train_dataset = ImageLidarDataset(train_list, args.sat_dir, args.mask_dir, args.lidar_dir, randomize=False,  mask_transform=mask_transform, adjust_resolution=adjust_resolution)
     val_dataset   = ImageLidarDataset(val_list,   args.sat_dir, args.mask_dir, args.lidar_dir, randomize=False, mask_transform=mask_transform, adjust_resolution=adjust_resolution)
     test_dataset  = ImageLidarDataset(test_list,  args.sat_dir, args.mask_dir, args.lidar_dir, randomize=False, mask_transform=mask_transform, adjust_resolution=adjust_resolution)
+
+    return train_dataset, val_dataset, test_dataset
+
+
+def prepare_porto_dataset(args):
+    print("")
+    print("Dataset: ", args.dataset)
+    print("down resolution: ", args.down_scale)
+
+    print("")
+    print("sat_dir: ", args.sat_dir)
+    print("gps_dir: ", args.gps_dir)
+    print("mask_dir: ", args.mask_dir)
+    print("")
+
+
+
+    train_list = val_list = test_list = []
+    with open(os.path.join(args.split_train_val_test, 'train.txt'), 'r') as f:
+        train_list = [x[:-1] for x in f]
+    with open(os.path.join(args.split_train_val_test, 'valid.txt'), 'r') as f:
+        val_list = [x[:-1] for x in f]
+    with open(os.path.join(args.split_train_val_test, 'test.txt'), 'r') as f:
+        test_list = [x[:-1] for x in f]
+
+    train_dataset = ImageGPSDataset(train_list, args.sat_dir, args.mask_dir, args.gps_dir, randomize=True,  down_scale=args.down_scale)
+    val_dataset = ImageGPSDataset(val_list, args.sat_dir, args.mask_dir, args.gps_dir, randomize=False,down_scale=args.down_scale)
+    test_dataset = ImageGPSDataset(test_list, args.sat_dir, args.mask_dir, args.gps_dir, randomize=False,  down_scale=args.down_scale)
 
     return train_dataset, val_dataset, test_dataset
 
