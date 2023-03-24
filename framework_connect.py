5643a65efbe95070ec201ed87cb2f8e18df93870
lcy80366872 <106853511+lcy80366872@users.noreply.github.com>
2023-03-24 21:56:25 +0800

    Update framework_connect.py

--- framework_connect.py
+++ framework_connect.py
@@ -35,10 +35,7 @@ def pre_general(output, out_connect, out_connect_d1):
     su[su > 0] = 1
 
     return torch.Tensor(su)
-##这个瞎写的代码必须是测试时batch——size=1才能用
 def pre_general_test(output, out_connect, out_connect_d1):
-#     print('out_connect:',out_connect.shape)
-#     print('output:',output.shape)
     out_connect_full = []
     out_connect = out_connect.data.cpu().numpy()
     out_connect_full.append(out_connect[0, ...])
@@ -46,11 +43,9 @@ def pre_general_test(output, out_connect, out_connect_d1):
     out_connect_full.append(out_connect[2, :, :, ::-1])
     out_connect_full.append(out_connect[3, :, ::-1, ::-1])
     out_connect_full = np.asarray(out_connect_full).mean(axis=0)[np.newaxis, :, :, :]
-#     print('out_connect_full:',out_connect_full.shape)
     pred_connect = np.sum(out_connect_full, axis=1)
     pred_connect[pred_connect < 0.9] = 0
     pred_connect[pred_connect >= 0.9] = 1
-#     print('pred_connect:',pred_connect.shape)
 
     out_connect_d1_full = []
     out_connect_d1 = out_connect_d1.data.cpu().numpy()
@@ -64,7 +59,8 @@ def pre_general_test(output, out_connect, out_connect_d1):
     pred_connect_d1[pred_connect_d1 >= 2.0] = 1
 
     pred_full = []
-    pred = output.data.cpu().numpy()
+    pred = output
+    # target_n = target.cpu().numpy()
     pred_full.append(pred[0, ...])
     pred_full.append(pred[1, :, ::-1, :])
     pred_full.append(pred[2, :, :, ::-1])
@@ -76,11 +72,50 @@ def pre_general_test(output, out_connect, out_connect_d1):
 
     su = pred_full + pred_connect + pred_connect_d1
     su[su > 0] = 1
-    su=su[np.newaxis, :, :, :]
-    su=su.astype(int)
-#     print('su_shape:',su.shape)
-    
+    print('su_shape:',su.shape)
     return torch.Tensor(su)
+def direction_process(general_mask):
+
+    img = general_mask
+    img[img >= 0.5] = 1
+    img[img < 0.5] = 0
+    shp = img.shape   #n*c*h*w
+    # print('shap:',shp)
+    # print('img:',img)
+
+    img_pad = torch.zeros([shp[0],shp[1],shp[2] + 2, shp[3] + 2])
+    # print('img:', img_pad.shape)
+    img_pad[:,:,1:-1, 1:-1] = img
+    # print('img:',img_pad.shape)
+    connect = torch.zeros([shp[0],9,shp[2], shp[3]])
+    #roll参数分别为输入、滚动距离和滚动维度
+    c1=torch.roll(img_pad,[1,1] ,[2,3])[:,:,1:-1,1:-1]
+    c2=torch.roll(img_pad,[1,0] ,[2,3])[:,:,1:-1,1:-1]
+    c3=torch.roll(img_pad,[1,-1] ,[2,3])[:,:,1:-1,1:-1]
+    c4=torch.roll(img_pad,[0,1] ,[2,3])[:,:,1:-1,1:-1]
+    c5=torch.roll(img_pad,[0,-1] ,[2,3])[:,:,1:-1,1:-1]
+    c6=torch.roll(img_pad,[-1,1] ,[2,3])[:,:,1:-1,1:-1]
+    c7=torch.roll(img_pad,[-1,0] ,[2,3])[:,:,1:-1,1:-1]
+    c8=torch.roll(img_pad,[-1,-1] ,[2,3])[:,:,1:-1,1:-1]
+    c1 = c1.cuda()
+    c2 = c2.cuda()
+    c3 = c3.cuda()
+    c4 = c4.cuda()
+    c5 = c5.cuda()
+    c6 = c6.cuda()
+    c7 = c7.cuda()
+    c8 = c8.cuda()
+    connect[:, 0, :, :] = img
+    connect[:, 1, :, :] = torch.where(img > 0, c1, img)
+    connect[:, 2, :, :] = torch.where(img > 0, c2, img)
+    connect[:, 3, :, :] = torch.where(img > 0, c3, img)
+    connect[:, 4, :, :] = torch.where(img > 0, c4, img)
+    connect[:, 5, :, :] = torch.where(img > 0, c5, img)
+    connect[:, 6, :, :] = torch.where(img > 0, c6, img)
+    connect[:, 7, :, :] = torch.where(img > 0, c7, img)
+    connect[:, 8, :, :] = torch.where(img > 0, c8, img)
+
+    return connect
 class Solver:
     def __init__(self, net, optimizer, dataset):
         self.net = torch.nn.DataParallel(net.cuda(), device_ids=list(range(torch.cuda.device_count())))
@@ -89,6 +124,7 @@ class Solver:
         self.dataset = dataset
 
         self.loss = dice_bce_loss()
+        self.loss_con=nn.BCEWithLogitsLoss()
         self.criterion_con = SegmentationLosses(weight=None, cuda=True).build_loss(mode='con_ce')
 
         self.metrics = IoU(threshold=0.5)
@@ -97,10 +133,8 @@ class Solver:
     def set_input(self, img_batch, mask_batch=None):
         self.img = img_batch
         self.mask = mask_batch[:,0,:,:].unsqueeze(1)
-
-
         self.connect_label=mask_batch[:,1:10,:,:]
-        self.connect_d1_label = mask_batch[:, 10:, :, :]
+        # self.connect_d1_label = mask_batch[:, 10:, :, :]
     def data2cuda(self, volatile=False):
         if volatile:
             with torch.no_grad():
@@ -113,11 +147,11 @@ class Solver:
                 with torch.no_grad():
                     self.mask = Variable(self.mask.cuda())
                     self.connect_label=Variable(self.connect_label.cuda())
-                    self.connect_d1_label = Variable(self.connect_d1_label.cuda())
+                    # self.connect_d1_label = Variable(self.connect_d1_label.cuda())
             else:
                 self.mask = Variable(self.mask.cuda())
                 self.connect_label = Variable(self.connect_label.cuda())
-                self.connect_d1_label = Variable(self.connect_d1_label.cuda())
+                # self.connect_d1_label = Variable(self.connect_d1_label.cuda())
 
 
     def optimize(self):
@@ -125,37 +159,56 @@ class Solver:
         self.data2cuda()
         
         self.optimizer.zero_grad()
-        pred,connect,connect_d1 = self.net.forward(self.img)
+        # pred,connect,connect_d1 = self.net.forward(self.img)
+        pred = self.net.forward(self.img)
+        #这里clone一下就是因为直接用pred进行操作那么在传播时会有问题
+        pred1=pred.clone()
         loss1 = self.loss(self.mask, pred)
+        connect=direction_process(pred1).cuda()
+        # print('connect:', connect.shape)
         loss2 = self.loss(self.connect_label,connect)
-        loss3 = self.loss(self.connect_d1_label,connect_d1 )
+        # loss3 = self.loss(self.connect_d1_label,connect_d1 )
         lad = 0.2
-        loss = loss1 + lad * (0.6 * loss2 + 0.4 * loss3)
+        loss=loss1+lad*loss2
+        # loss = loss1 + lad * (0.6 * loss2 + 0.4 * loss3)
         loss.backward()
         self.optimizer.step()
-#         pred=pre_general(pred,connect,connect_d1)
+        # pred=pre_general(pred,connect,connect_d1)
         batch_iou, intersection, union = self.metrics(self.mask, pred)
         return pred, loss.item(), batch_iou, intersection, union
 
     def test_batch(self):
         self.net.eval()
         self.data2cuda(volatile=True)
-        pred,connect,connect_d1  = self.net.forward(self.img)
+
+        pred = self.net.forward(self.img)
         loss1 = self.loss(self.mask, pred)
-        loss2 = self.loss(self.connect_label,connect )
-        loss3 = self.loss( self.connect_d1_label,connect_d1)
+        pred1 = pred.clone()
+        connect = direction_process(pred1).cuda()
+        loss2 = self.loss(self.connect_label,connect)
         lad = 0.2
-        loss = loss1 + lad * (0.6 * loss2 + 0.4 * loss3)
-        img = self.img.cpu().numpy()
-        image1 = img[:, :, ::-1, :]
-        image2 = img[:, :, :, ::-1]
-        image3 = img[:, :, ::-1, ::-1]
-        img = np.concatenate((img, image1, image2, image3), axis=0)
-        img = torch.from_numpy(img).float()
-        pred, connect, connect_d1 = self.net.forward(img)
-        pred = pre_general_test(pred, connect, connect_d1)
+        loss = loss1 + lad * loss2
+
         batch_iou, intersection, union = self.metrics(self.mask, pred)
         pred = pred.cpu().data.numpy().squeeze(1)
+
+        # pred,connect,connect_d1  = self.net.forward(self.img)
+        # loss1 = self.loss(self.mask, pred)
+        # loss2 = self.loss(self.connect_label,connect )
+        # # loss3 = self.loss( self.connect_d1_label,connect_d1)
+        # lad = 0.2
+        # loss=loss1+lad*loss2
+        # # loss = loss1 + lad * (0.6 * loss2 + 0.4 * loss3)
+        # img = self.img.cpu().numpy()
+        # image1 = img[:, :, ::-1, :]
+        # image2 = img[:, :, :, ::-1]
+        # image3 = img[:, :, ::-1, ::-1]
+        # img = np.concatenate((img, image1, image2, image3), axis=0)
+        # img = torch.from_numpy(img).float()
+        # pred, connect, connect_d1 = self.net.forward(img)
+        # pred = pre_general_test(pred, connect, connect_d1)
+        # batch_iou, intersection, union = self.metrics(self.mask, pred)
+        # pred = pred.cpu().data.numpy().squeeze(1)
         return pred, loss.item(), batch_iou, intersection, union
         
         
@@ -233,17 +286,18 @@ class Framework:
         
         dataloader_iter = iter(dataloader) 
         iter_num = len(dataloader_iter)
+        # print('iter_num:',iter_num)
         progress_bar = tqdm(enumerate(dataloader_iter), total=iter_num)
         
         for i, (img, mask) in progress_bar:
-           
+            # print('mask:',mask.shape)
             self.solver.set_input(img, mask)
             if mode=='training':
                 pred_map, iter_loss, batch_iou, samples_intersection, samples_union = self.solver.optimize()
             else:
-                
+
                 pred_map, iter_loss, batch_iou, samples_intersection, samples_union = self.solver.test_batch()
-                
+
             epoch_loss += iter_loss
             progress_bar.set_description(f'{mode} iter: {i} loss: {iter_loss:.4f}')
             
