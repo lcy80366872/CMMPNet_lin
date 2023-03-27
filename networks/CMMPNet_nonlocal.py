ea29cc75c1e3bff2ddb89abc4b8ec0138becd6a2
lcy80366872 <106853511+lcy80366872@users.noreply.github.com>
2023-03-27 11:10:37 +0800

    Update and rename CMMPNet_spin.py to CMMPNet_nonlocal.py

--- networks/CMMPNet_nonlocal.py
+++ networks/CMMPNet_nonlocal.py
@@ -1,156 +1,12 @@
 import torch
-import torch.nn as nn
 from torchvision import models
 from .basic_blocks import *
 import math
 import torch.nn.functional as F
 from networks.MPM import SPHead
-
-
+from networks.Nonlocal import NLBlockND
 up_kwargs = {'mode': 'bilinear', 'align_corners': True}
 
-BatchNorm2d = nn.BatchNorm2d
-BatchNorm1d = nn.BatchNorm1d
-
-
-
-class SpatialGCN(nn.Module):
-    """
-        Spatial Space Graph Reasoning ...
-    """
-    def __init__(self, plane):
-        super(SpatialGCN, self).__init__()
-        inter_plane = plane // 2
-        self.node_k = nn.Conv2d(plane, inter_plane, kernel_size=1)
-        self.node_v = nn.Conv2d(plane, inter_plane, kernel_size=1)
-        self.node_q = nn.Conv2d(plane, inter_plane, kernel_size=1)
-
-        self.conv_wg = nn.Conv1d(inter_plane, inter_plane, kernel_size=1, bias=False)
-        self.bn_wg = BatchNorm1d(inter_plane)
-        self.softmax = nn.Softmax(dim=2)
-
-        self.out = nn.Sequential(nn.Conv2d(inter_plane, plane, kernel_size=1),
-                                 BatchNorm2d(plane))
-
-    def forward(self, x):
-        # b, c, h, w = x.size()
-        node_k = self.node_k(x)
-        node_v = self.node_v(x)
-        node_q = self.node_q(x)
-        b,c,h,w = node_k.size()
-        node_k = node_k.view(b, c, -1).permute(0, 2, 1)
-        node_q = node_q.view(b, c, -1)
-        node_v = node_v.view(b, c, -1).permute(0, 2, 1)
-        # A = k * q
-        # AV = k * q * v
-        # AVW = k *(q *v) * w
-        AV = torch.bmm(node_q,node_v)
-        AV = self.softmax(AV)
-        AV = torch.bmm(node_k, AV)
-        AV = AV.transpose(1, 2).contiguous()
-        AVW = self.conv_wg(AV)
-        AVW = self.bn_wg(AVW)
-        AVW = AVW.view(b, c, h, -1)
-
-        out = F.relu_(self.out(AVW) + x)
-
-        return out
-
-
-
-class spin(nn.Module):
-    """
-        Spatial and Interaction Space Graph Reasoning ...
-    """
-    def __init__(self, planes, ratio=4):
-        super(spin, self).__init__()
-
-        self.phi = nn.Conv2d(planes, planes // ratio * 2, kernel_size=1, bias=False)
-        self.bn_phi = BatchNorm2d(planes // ratio * 2)
-        self.theta = nn.Conv2d(planes, planes // ratio, kernel_size=1, bias=False)
-        self.bn_theta = BatchNorm2d(planes // ratio)
-
-        #  Interaction Space
-        #  Adjacency Matrix: (-)A_g
-        self.conv_adj = nn.Conv1d(planes // ratio, planes // ratio, kernel_size=1, bias=False)
-        self.bn_adj = BatchNorm1d(planes // ratio)
-
-        #  State Update Function: W_g
-        self.conv_wg = nn.Conv1d(planes // ratio * 2, planes // ratio * 2, kernel_size=1, bias=False)
-        self.bn_wg = BatchNorm1d(planes // ratio * 2)
-
-        #  last fc
-        self.conv3 = nn.Conv2d(planes // ratio * 2, planes, kernel_size=1, bias=False)
-        self.bn3 = BatchNorm2d(planes)
-
-        self.local = nn.Sequential(
-            nn.Conv2d(planes, planes, 3, groups=planes, stride=2, padding=1, bias=False),
-            BatchNorm2d(planes),
-            nn.Conv2d(planes, planes, 3, groups=planes, stride=2, padding=1, bias=False),
-            BatchNorm2d(planes),
-            nn.Conv2d(planes, planes, 3, groups=planes, stride=2, padding=1, bias=False),
-            BatchNorm2d(planes))
-        self.gcn_local_attention = SpatialGCN(planes)
-
-        self.final = nn.Sequential(nn.Conv2d(planes * 2, planes, kernel_size=1, bias=False),
-                                   BatchNorm2d(planes))
-
-    def to_matrix(self, x):
-        n, c, h, w = x.size()
-        x = x.view(n, c, -1)
-        return x
-
-    def forward(self, feat):
-        # # # # Local # # # #
-        x = feat
-        local = self.local(feat)
-        local = self.gcn_local_attention(local)
-        local = F.interpolate(local, size=x.size()[2:], mode='bilinear', align_corners=True)
-        spatial_local_feat = x * local + x
-
-        # # # # Projection Space # # # #
-        x_sqz, b = x, x
-
-        x_sqz = self.phi(x_sqz)
-        x_sqz = self.bn_phi(x_sqz)
-        x_sqz = self.to_matrix(x_sqz)
-
-        b = self.theta(b)
-        b = self.bn_theta(b)
-        b = self.to_matrix(b)
-
-        # Project
-        z_idt = torch.matmul(x_sqz, b.transpose(1, 2))
-
-        # # # # Interaction Space # # # #
-        z = z_idt.transpose(1, 2).contiguous()
-
-        z = self.conv_adj(z)
-        z = self.bn_adj(z)
-
-        z = z.transpose(1, 2).contiguous()
-        # Laplacian smoothing: (I - A_g)Z => Z - A_gZ
-        z += z_idt
-
-        z = self.conv_wg(z)
-        z = self.bn_wg(z)
-
-        # # # # Re-projection Space # # # #
-        # Re-project
-        y = torch.matmul(z, b)
-
-        n, _, h, w = x.size()
-        y = y.view(n, -1, h, w)
-
-        y = self.conv3(y)
-        y = self.bn3(y)
-
-        g_out = F.relu_(x+y)
-
-        # Sum features ...
-        out = self.final(torch.cat((spatial_local_feat, g_out), 1))
-
-        return out
 
 # spp只到拼接，后面的fc层步骤不算在spp内
 class SPPLayer(torch.nn.Module):
@@ -261,6 +117,7 @@ class DinkNet34_CMMPNet(nn.Module):
 
         self.encoder1 = resnet.layer1
         self.encoder2 = resnet.layer2
+        self.non_local= NLBlockND(filters[1], mode='embedded', dimension=2)
         self.encoder3 = resnet.layer3
         self.encoder4 = resnet.layer4
 
@@ -272,15 +129,8 @@ class DinkNet34_CMMPNet(nn.Module):
         self.decoder2 = DecoderBlock(filters[1], filters[0])
         self.decoder1 = DecoderBlock(filters[0], filters[0])
 
-        self.maxpool = nn.MaxPool2d(2, stride=2, ceil_mode=True)
         self.finaldeconv1 = nn.ConvTranspose2d(filters[0], filters[0] // 2, 4, 2, 1)
         self.finalrelu1 = nonlinearity
-
-        # Dual Graph Convolutional module
-        self.dgcn_seg1 = spin(filters[0] // 2, ratio=2)
-        self.dgcn_seg2 = spin(filters[0] // 2, ratio=2)
-        self.dgcn_seg3 = spin(filters[0] // 2, ratio=2)
-
         self.finalconv2 = nn.Conv2d(filters[0] // 2, filters[0] // 2, 3, padding=1)
         self.finalrelu2 = nonlinearity
 
@@ -335,9 +185,13 @@ class DinkNet34_CMMPNet(nn.Module):
         add_e1 = self.encoder1_add(add)
         x_e1, add_e1 = self.dem_e1(x_e1, add_e1)
 
+
         x_e2 = self.encoder2(x_e1)
         add_e2 = self.encoder2_add(add_e1)
+        x_e2 =self.non_local(x_e2)
+        add_e2=self.non_local(add_e2)
         x_e2, add_e2 = self.dem_e2(x_e2, add_e2)
+
 
         x_e3 = self.encoder3(x_e2)
         add_e3 = self.encoder3_add(add_e2)
@@ -348,8 +202,6 @@ class DinkNet34_CMMPNet(nn.Module):
         x_e4, add_e4 = self.dem_e4(x_e4, add_e4)
 
         # Center
-        # x_e4 = self.head(x_e4)
-        # add_e4 = self.head(add_e4)
         x_c = self.dblock(x_e4)
         add_c = self.dblock_add(add_e4)
         # 传递增强信息时还有跳跃连接
@@ -370,39 +222,12 @@ class DinkNet34_CMMPNet(nn.Module):
         add_d1 = self.decoder1_add(add_d2)
         x_d1, add_d1 = self.dem_d1(x_d1, add_d1)
 
-
         x_out = self.finalrelu1(self.finaldeconv1(x_d1))
         add_out = self.finalrelu1_add(self.finaldeconv1_add(add_d1))
-
-        # SPIN Pyramid
-        spin257 = self.dgcn_seg1(x_out)  # SPIN at 257x257 scale
-        f1_128 = self.maxpool(x_out)
-
-        spin128 = self.dgcn_seg2(f1_128)  # SPIN at 128*128 scale
-        f1_64 = self.maxpool(f1_128)
-        spin64 = self.dgcn_seg3(f1_64)  # SPIN at 64*64 scale
-        spin64_up = F.interpolate(spin64, size=(f1_128.shape[2], f1_128.shape[3]), mode="bilinear")
-        spin128_comb = torch.add(spin64_up, spin128)
-        spin128_up = F.interpolate(spin128_comb, size=(spin257.shape[2], spin257.shape[3]), mode="bilinear")
-        f1 = torch.add(spin128_up, spin257)
-
-        # SPIN Pyramid_add
-        spin257 = self.dgcn_seg1(add_out)  # SPIN at 257x257 scale
-        f2_128 = self.maxpool(add_out)
-        spin128 = self.dgcn_seg2(f2_128)  # SPIN at 128*128 scale
-        f2_64 = self.maxpool(f2_128)
-        spin64 = self.dgcn_seg3(f2_64)  # SPIN at 64*64 scale
-        spin64_up = F.interpolate(spin64, size=(f2_128.shape[2], f2_128.shape[3]), mode="bilinear")
-        spin128_comb = torch.add(spin64_up, spin128)
-        spin128_up = F.interpolate(spin128_comb, size=(spin257.shape[2], spin257.shape[3]), mode="bilinear")
-        f2 = torch.add(spin128_up, spin257)
-
-        x_out = self.finalrelu2(self.finalconv2(f1))
-        add_out = self.finalrelu2_add(self.finalconv2_add(f2))
+        x_out = self.finalrelu2(self.finalconv2(x_out))
+        add_out = self.finalrelu2_add(self.finalconv2_add(add_out))
 
         out = self.finalconv(torch.cat((x_out, add_out), 1))  # b*1*h*w
         return torch.sigmoid(out)
-
-
 
 
