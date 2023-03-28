09ba878a653cf0a589064a1e3a3dc806fb919332
lcy80366872 <106853511+lcy80366872@users.noreply.github.com>
2023-03-28 17:21:53 +0800

    Create CondConv.py

---
+++ networks/CondConv.py
@@ -0,0 +1,89 @@
+import torch
+from torch import nn
+from torch.nn import functional as F
+
+class Attention(nn.Module):
+    def __init__(self,in_planes,K,init_weight=True):
+        super().__init__()
+        self.avgpool=nn.AdaptiveAvgPool2d(1)
+        self.net=nn.Conv2d(in_planes,K,kernel_size=1,bias=False)
+        self.sigmoid=nn.Sigmoid()
+
+        if(init_weight):
+            self._initialize_weights()
+
+
+    def _initialize_weights(self):
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
+                if m.bias is not None:
+                    nn.init.constant_(m.bias, 0)
+            if isinstance(m ,nn.BatchNorm2d):
+                nn.init.constant_(m.weight, 1)
+                nn.init.constant_(m.bias, 0)
+
+    def forward(self,x):
+        att=self.avgpool(x) #bs,dim,1,1
+        att=self.net(att).view(x.shape[0],-1) #bs,K
+        return self.sigmoid(att)
+
+class CondConv(nn.Module):
+    def __init__(self,in_planes,out_planes,kernel_size,stride,padding=0,dilation=1,grounps=1,bias=True,K=4,init_weight=True):
+        super().__init__()
+        self.in_planes=in_planes
+        self.out_planes=out_planes
+        self.kernel_size=kernel_size
+        self.stride=stride
+        self.padding=padding
+        self.dilation=dilation
+        self.groups=grounps
+        self.bias=bias
+        self.K=K
+        self.init_weight=init_weight
+        self.attention=Attention(in_planes=in_planes,K=K,init_weight=init_weight)
+
+        self.weight=nn.Parameter(torch.randn(K,out_planes,in_planes//grounps,kernel_size,kernel_size),requires_grad=True)
+        if(bias):
+            self.bias=nn.Parameter(torch.randn(K,out_planes),requires_grad=True)
+        else:
+            self.bias=None
+        
+        if(self.init_weight):
+            self._initialize_weights()
+
+        #TODO 初始化
+    def _initialize_weights(self):
+        for i in range(self.K):
+            nn.init.kaiming_uniform_(self.weight[i])
+
+    def forward(self,input,x):
+        # 调用 attention 函数得到归一化的权重 [N, K]
+        bs,in_planels,h,w=x.shape
+
+        softmax_att=self.attention(x) #bs,K
+        # 把输入特征由 [N, C_in, H, W] 转化为 [1, N*C_in, H, W]
+        x=x.view(1,-1,h,w)
+        # 生成随机 weight [K, C_out, C_in/groups, 3, 3] (卷积核一般为3*3)
+        # 注意添加了 requires_grad=True，这样里面的参数是可以优化的
+        # 改变 weight 形状为 [K, C_out*(C_in/groups)*3*3]
+        weight=self.weight.view(self.K,-1) #K,-1
+        # 矩阵相乘：[N, K] X [K, C_out*(C_in/groups)*3*3] = [N, C_out*(C_in/groups)*3*3]
+        # 改变形状为：[N*C_out, C_in/groups, 3, 3]，即新的卷积核权重
+        aggregate_weight=torch.mm(softmax_att,weight).view(bs*self.out_planes,self.in_planes//self.groups,self.kernel_size,self.kernel_size) #bs*out_p,in_p,k,k
+        # 用新生成的卷积核进行卷积，输出为 [1, N*C_out, H, W]
+        if(self.bias is not None):
+            bias=self.bias.view(self.K,-1) #K,out_p
+            aggregate_bias=torch.mm(softmax_att,bias).view(-1) #bs,out_p
+            output=F.conv2d(input,weight=aggregate_weight,bias=aggregate_bias,stride=self.stride,padding=self.padding,groups=self.groups*bs,dilation=self.dilation)
+        else:
+            output=F.conv2d(input,weight=aggregate_weight,bias=None,stride=self.stride,padding=self.padding,groups=self.groups*bs,dilation=self.dilation)
+        # 形状恢复为 [N, C_out, H, W]
+        output=output.view(bs,self.out_planes,h,w)
+        return output
+
+if __name__ == '__main__':
+    input=torch.randn(2,32,64,64)
+    m=CondConv(in_planes=32,out_planes=64,kernel_size=3,stride=1,padding=1,bias=False)
+    out=m(input)
+    print(out.shape)
