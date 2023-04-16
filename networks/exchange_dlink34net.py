c18f0b36653fdfaeabff7fcf575b6b5261dbbd73
lcy80366872 <106853511+lcy80366872@users.noreply.github.com>
2023-04-16 20:22:23 +0800

    Update exchange_dlink34net.py

--- networks/exchange_dlink34net.py
+++ networks/exchange_dlink34net.py
@@ -214,7 +214,7 @@ class ResNet(nn.Module):
         # self.atten=CBAMBlock(channel=filters[0], reduction=4, kernel_size=7)
         # self.fuse =NLBlockND_Fuse(filters[0]//2, filters[0]//2,mode='embedded', dimension=2)
         # self.fuse =CrissCrossAttention_Fuse(filters[0])
-        self.finalconv = nn.Conv2d(2*filters[0], num_classes, 3, padding=1)
+        self.finalconv = nn.Conv2d(filters[0], num_classes, 3, padding=1)
         # self.finalconv = ModuleParallel(nn.Conv2d(filters[0] // 2, num_classes, 3, padding=1))
         # self.alpha = nn.Parameter(torch.ones(num_parallel, requires_grad=True))
         # self.register_parameter('alpha', self.alpha)
@@ -243,7 +243,6 @@ class ResNet(nn.Module):
 
         x = inputs[:, :3, :, :]
         g = inputs[:, 3:4, :, :]
-        print('xxxxxxxx',ycbr.shape)
         ycbr_dct=ycbr[:,64:,:,:]
         gps_dct = ycbr[:, :64, :, :]
 
