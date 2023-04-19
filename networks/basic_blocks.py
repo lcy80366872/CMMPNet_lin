fff9e34cd4da38fa343b04d5197c39141504c173
lcy80366872 <106853511+lcy80366872@users.noreply.github.com>
2023-04-19 10:39:17 +0800

    Update basic_blocks.py

--- networks/basic_blocks.py
+++ networks/basic_blocks.py
@@ -295,10 +295,9 @@ class DecoderBlock_parallel_exchange(nn.Module):
         x = self.bn3(x)
         x = self.relu3(x)
         return x
+BatchNorm2d = nn.BatchNorm2d
+BatchNorm1d = nn.BatchNorm1d
 class SpatialGCN(nn.Module):
-    """
-        Spatial Space Graph Reasoning ...
-    """
     def __init__(self, plane):
         super(SpatialGCN, self).__init__()
         inter_plane = plane // 2
@@ -332,20 +331,16 @@ class SpatialGCN(nn.Module):
         AVW = self.conv_wg(AV)
         AVW = self.bn_wg(AVW)
         AVW = AVW.view(b, c, h, -1)
-
         out = F.relu_(self.out(AVW) + x)
-
         return out
 
 
-BatchNorm2d = nn.BatchNorm2d
-BatchNorm1d = nn.BatchNorm1d
-class spin(nn.Module):
+class DualGCN(nn.Module):
     """
-        Spatial and Interaction Space Graph Reasoning ...
+        Feature GCN with coordinate GCN
     """
     def __init__(self, planes, ratio=4):
-        super(spin, self).__init__()
+        super(DualGCN, self).__init__()
 
         self.phi = nn.Conv2d(planes, planes // ratio * 2, kernel_size=1, bias=False)
         self.bn_phi = BatchNorm2d(planes // ratio * 2)
@@ -429,11 +424,10 @@ class spin(nn.Module):
 
         g_out = F.relu_(x+y)
 
-        # Sum features ...
+        # cat or sum, nearly the same results
         out = self.final(torch.cat((spatial_local_feat, g_out), 1))
 
         return out
-
 
 class DecoderBlock(nn.Module):
     def __init__(self, in_channels, n_filters):
@@ -462,117 +456,4 @@ class DecoderBlock(nn.Module):
         x = self.norm3(x)
         x = self.relu3(x)
         return x
-
-
-class DecoderBlock1DConv2(nn.Module):
-    def __init__(self, in_channels, n_filters):
-        super(DecoderBlock1DConv2, self).__init__()
-        self.conv1 = nn.Conv2d(in_channels, in_channels // 4, 1)
-        self.norm1 = nn.BatchNorm2d(in_channels // 4)
-        self.relu1 = nonlinearity
-        self.deconv1 = nn.ConvTranspose2d(
-            in_channels // 4, in_channels // 8, (1, 9), padding=(0, 4)
-        )
-        self.deconv2 = nn.ConvTranspose2d(
-            in_channels // 4, in_channels // 8, (9, 1), padding=(4, 0)
-        )
-        self.norm2 = nn.BatchNorm2d(in_channels // 4)
-        self.relu2 = nonlinearity
-        self.conv3 = nn.Conv2d(in_channels // 4, n_filters, 1)
-        self.norm3 = nn.BatchNorm2d(n_filters)
-        self.relu3 = nonlinearity
-
-    def forward(self, x):
-        x = self.conv1(x)
-        x = self.norm1(x)
-        x = self.relu1(x)
-        x1 = self.deconv1(x)
-        x2 = self.deconv2(x)
-        x = torch.cat((x1, x2), 1)
-
-        x = F.interpolate(x, scale_factor=2)
-        x = self.norm2(x)
-        x = self.relu2(x)
-        x = self.conv3(x)
-        x = self.norm3(x)
-        x = self.relu3(x)
-        return x
-
-
-class DecoderBlock1DConv4(nn.Module):
-    def __init__(self, in_channels, n_filters):
-        super(DecoderBlock1DConv4, self).__init__()
-        self.conv1 = nn.Conv2d(in_channels, in_channels // 4, 1)
-        self.norm1 = nn.BatchNorm2d(in_channels // 4)
-        self.relu1 = nonlinearity
-
-        self.deconv1 = nn.ConvTranspose2d(
-            in_channels // 4, in_channels // 8, (1, 9), padding=(0, 4)
-        )
-        self.deconv2 = nn.ConvTranspose2d(
-            in_channels // 4, in_channels // 8, (9, 1), padding=(4, 0)
-        )
-        self.deconv3 = nn.ConvTranspose2d(
-            in_channels // 4, in_channels // 16, (9, 1), padding=(4, 0)
-        )
-        self.deconv4 = nn.ConvTranspose2d(
-            in_channels // 4, in_channels // 16, (1, 9), padding=(0, 4)
-        )
-
-        self.norm2 = nn.BatchNorm2d(in_channels // 4 + in_channels // 8)
-        self.relu2 = nonlinearity
-        self.conv3 = nn.Conv2d(
-            in_channels // 4 + in_channels // 8, n_filters, 1)
-        self.norm3 = nn.BatchNorm2d(n_filters)
-        self.relu3 = nonlinearity
-
-    def forward(self, x):
-        x = self.conv1(x)
-        x = self.norm1(x)
-        x = self.relu1(x)
-        x1 = self.deconv1(x)
-        x2 = self.deconv2(x)
-        x3 = self.inv_h_transform(self.deconv3(self.h_transform(x)))
-        x4 = self.inv_v_transform(self.deconv4(self.v_transform(x)))
-        x = torch.cat((x1, x2, x3, x4), 1)
-
-        x = F.interpolate(x, scale_factor=2)
-        x = self.norm2(x)
-        x = self.relu2(x)
-        x = self.conv3(x)
-        x = self.norm3(x)
-        x = self.relu3(x)
-        return x
-
-    def h_transform(self, x):
-        shape = x.size()
-        x = torch.nn.functional.pad(x, (0, shape[-1]))
-        x = x.reshape(shape[0], shape[1], -1)[..., :-shape[-1]]
-        x = x.reshape(shape[0], shape[1], shape[2], 2*shape[3]-1)
-        return x
-
-    def inv_h_transform(self, x):
-        shape = x.size()
-        x = x.reshape(shape[0], shape[1], -1).contiguous()
-        x = torch.nn.functional.pad(x, (0, shape[-2]))
-        x = x.reshape(shape[0], shape[1], shape[-2], 2*shape[-2])
-        x = x[..., 0: shape[-2]]
-        return x
-
-    def v_transform(self, x):
-        x = x.permute(0, 1, 3, 2)
-        shape = x.size()
-        x = torch.nn.functional.pad(x, (0, shape[-1]))
-        x = x.reshape(shape[0], shape[1], -1)[..., :-shape[-1]]
-        x = x.reshape(shape[0], shape[1], shape[2], 2*shape[3]-1)
-        return x.permute(0, 1, 3, 2)
-
-    def inv_v_transform(self, x):
-        x = x.permute(0, 1, 3, 2)
-        shape = x.size()
-        x = x.reshape(shape[0], shape[1], -1)
-        x = torch.nn.functional.pad(x, (0, shape[-2]))
-        x = x.reshape(shape[0], shape[1], shape[-2], 2*shape[-2])
-        x = x[..., 0: shape[-2]]
-        return x.permute(0, 1, 3, 2)
 
