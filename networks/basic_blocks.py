23f2f1c0b6cc82acfa45ac06b3710780a25cf5c3
lcy80366872 <106853511+lcy80366872@users.noreply.github.com>
2023-04-23 16:25:30 +0800

    Update basic_blocks.py

--- networks/basic_blocks.py
+++ networks/basic_blocks.py
@@ -31,7 +31,7 @@ class Exchange(nn.Module):
         x1[:, bn1 < bn_threshold] = x[1][:, bn1 < bn_threshold]
         x2[:, bn2 >= bn_threshold] = x[1][:, bn2 >= bn_threshold]
         x2[:, bn2 < bn_threshold] = x[0][:, bn2 < bn_threshold]
-        
+        print('bn',bn1 < bn_threshold)
 
         return [x1, x2]
 class ModuleParallel(nn.Module):
@@ -768,6 +768,86 @@ class DeformConv2d(nn.Module):
         x_offset = x_offset.contiguous().view(b, c, h*ks, w*ks)
 
         return x_offset
+
+class RefUnet(nn.Module):
+    def __init__(self,in_ch):
+        super(RefUnet, self).__init__()
+
+        self.conv0 = nn.Conv2d(in_ch,in_ch,3,padding=1)
+
+        self.conv1 = nn.Conv2d(in_ch,16,3,padding=1)
+        self.bn1 = nn.BatchNorm2d(16)
+        self.relu1 = nn.ReLU(inplace=True)
+
+        self.pool1 = nn.MaxPool2d(2,2,ceil_mode=True)
+
+        self.conv2 = nn.Conv2d(16,32,3,padding=1)
+        self.bn2 = nn.BatchNorm2d(32)
+        self.relu2 = nn.ReLU(inplace=True)
+
+        self.pool2 = nn.MaxPool2d(2,2,ceil_mode=True)
+
+        self.conv3 = nn.Conv2d(32,64,3,padding=1)
+        self.bn3 = nn.BatchNorm2d(64)
+        self.relu3 = nn.ReLU(inplace=True)
+
+        self.pool3 = nn.MaxPool2d(2,2,ceil_mode=True)
+
+        #####
+        self.conv4 = nn.Conv2d(64,64,3,padding=1)
+        self.bn4 = nn.BatchNorm2d(64)
+        self.relu4 = nn.ReLU(inplace=True)
+        #####
+
+        self.conv_d4 = nn.Conv2d(128,64,3,padding=1)
+        self.bn_d4 = nn.BatchNorm2d(64)
+        self.relu_d4 = nn.ReLU(inplace=True)
+
+        self.conv_d3 = nn.Conv2d(128,64,3,padding=1)
+        self.bn_d3 = nn.BatchNorm2d(64)
+        self.relu_d3 = nn.ReLU(inplace=True)
+
+        self.conv_d2 = nn.Conv2d(64+32,32,3,padding=1)
+        self.bn_d2 = nn.BatchNorm2d(32)
+        self.relu_d2 = nn.ReLU(inplace=True)
+
+        self.conv_d1 = nn.Conv2d(32+16,16,3,padding=1)
+        self.bn_d1 = nn.BatchNorm2d(16)
+        self.relu_d1 = nn.ReLU(inplace=True)
+
+        self.conv_d0 = nn.Conv2d(16,1,3,padding=1)
+
+        self.upscore2 = nn.Upsample(scale_factor=2, mode='bilinear')
+
+
+    def forward(self,x):
+
+        hx = x
+        hx = self.conv0(hx)
+
+        hx1 = self.relu1(self.bn1(self.conv1(hx))) #scale:1
+        hx = self.pool1(hx1) #scale:1/2
+
+        hx2 = self.relu2(self.bn2(self.conv2(hx)))
+        hx = self.pool2(hx2) #scale:1/4
+
+        hx3 = self.relu3(self.bn3(self.conv3(hx)))
+        hx = self.pool3(hx3) #scale:1/8
+
+        hx4 = self.relu4(self.bn4(self.conv4(hx)))
+        hx = self.upscore2(hx4)
+
+        d3 = self.relu_d3(self.bn_d3(self.conv_d3(torch.cat((hx,hx3),1))))
+        hx = self.upscore2(d3)
+
+        d2 = self.relu_d2(self.bn_d2(self.conv_d2(torch.cat((hx,hx2),1))))
+        hx = self.upscore2(d2)
+
+        d1 = self.relu_d1(self.bn_d1(self.conv_d1(torch.cat((hx,hx1),1))))
+
+        residual = self.conv_d0(d1)
+
+        return torch.sigmoid(x + residual)
 
 
 
