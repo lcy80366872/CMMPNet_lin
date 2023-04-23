d5492e077c21c9c63dd99af36d6f22834efb4fa5
lcy80366872 <106853511+lcy80366872@users.noreply.github.com>
2023-04-23 16:14:06 +0800

    Update loss.py

--- loss.py
+++ loss.py
@@ -2,6 +2,10 @@ import torch
 import torch.nn as nn
 import cv2
 import numpy as np
+import torch.nn.functional as F
+from torch.autograd import Variable
+import numpy as np
+from math import exp
 
 
 class SegmentationLosses(object):
@@ -18,14 +22,14 @@ class SegmentationLosses(object):
             return self.CrossEntropyLoss
         elif mode == 'focal':
             return self.FocalLoss
-        elif mode =='con_ce':
+        elif mode == 'con_ce':
             return self.ConLoss
         else:
             raise NotImplementedError
 
     def CrossEntropyLoss(self, logit, target):
         n, c, h, w = logit.size()
-        criterion = nn.CrossEntropyLoss(weight=self.weight, #ignore_index=self.ignore_index,
+        criterion = nn.CrossEntropyLoss(weight=self.weight,  # ignore_index=self.ignore_index,
                                         size_average=self.size_average)
         if self.cuda:
             criterion = criterion.cuda()
@@ -46,7 +50,7 @@ class SegmentationLosses(object):
 
     def FocalLoss(self, logit, target, gamma=2, alpha=0.25):
         n, c, h, w = logit.size()
-        criterion = nn.CrossEntropyLoss(weight=self.weight, #ignore_index=self.ignore_index,
+        criterion = nn.CrossEntropyLoss(weight=self.weight,  # ignore_index=self.ignore_index,
                                         size_average=self.size_average)
         if self.cuda:
             criterion = criterion.cuda()
@@ -62,14 +66,16 @@ class SegmentationLosses(object):
 
         return loss
 
+
 class dice_bce_loss(nn.Module):
     def __init__(self, batch=True):
         super(dice_bce_loss, self).__init__()
         self.batch = batch
         self.bce_loss = nn.BCELoss()
+        self.ssim=SSIM(window_size=11,size_average=True)
 
     def soft_dice_coeff(self, y_true, y_pred):
-        smooth = 1.0 
+        smooth = 1.0
         if self.batch:
             i = torch.sum(y_true)
             j = torch.sum(y_pred)
@@ -87,28 +93,151 @@ class dice_bce_loss(nn.Module):
 
     def resize(self, y_true, h, w):
         b = y_true.shape[0]
-        y = np.zeros((b, h,w ,y_true.shape[1]))
-        
+        y = np.zeros((b, h, w, y_true.shape[1]))
+        # print('y_t:', y_true.shape)
         y_true = np.array(y_true.cpu())
         for id in range(b):
-            y1 = y_true[id,:,:,:].transpose(1,2,0)
-#             print('y1:',y1.shape)
+            y1 = y_true[id, :, :, :].transpose(1, 2, 0)
+            # print('y1:', y1.shape)
             a = cv2.resize(y1, (h, w))
+
             if a.ndim == 2:
-                a=np.expand_dims(a,axis=-1)
-#             print('a:',a.shape)
-#             print('y:',y.shape)
-            y[id, :, :, :]=a
-        y=y.transpose(0,3,1,2)
+                a = np.expand_dims(a, axis=-1)
+            # print('a:', a.shape)
+            y[id, :, :, :] = a
+        # print(y.shape)
+        y = y.transpose(0, 3, 1,2)
+
         return torch.Tensor(y)
-        
+
     def __call__(self, y_true, y_pred):
         # the ground_truth map is resized to the resolution of the predicted map during training
         if y_true.shape[2] != y_pred.shape[2] or y_true.shape[3] != y_pred.shape[3]:
-            
             y_true = self.resize(y_true, y_pred.shape[2], y_pred.shape[3]).cuda()
-#             print(y_true.shape)
-           
+        print(y_pred)
+
+
         a = self.bce_loss(y_pred, y_true)
         b = self.soft_dice_loss(y_true, y_pred)
-        return a + b
+        c =  self.ssim(y_true, y_pred)
+
+        return a + b + c
+
+def gaussian(window_size, sigma):
+    gauss = torch.Tensor([exp(-(x - window_size//2)**2/float(2*sigma**2)) for x in range(window_size)])
+    return gauss/gauss.sum()
+
+def create_window(window_size, channel):
+    _1D_window = gaussian(window_size, 1.5).unsqueeze(1)
+    _2D_window = _1D_window.mm(_1D_window.t()).float().unsqueeze(0).unsqueeze(0)
+    window = Variable(_2D_window.expand(channel, 1, window_size, window_size).contiguous())
+    return window
+
+def _ssim(img1, img2, window, window_size, channel, size_average = True):
+    mu1 = F.conv2d(img1, window, padding = window_size//2, groups = channel)
+    mu2 = F.conv2d(img2, window, padding = window_size//2, groups = channel)
+
+    mu1_sq = mu1.pow(2)
+    mu2_sq = mu2.pow(2)
+    mu1_mu2 = mu1*mu2
+
+    sigma1_sq = F.conv2d(img1*img1, window, padding = window_size//2, groups = channel) - mu1_sq
+    sigma2_sq = F.conv2d(img2*img2, window, padding = window_size//2, groups = channel) - mu2_sq
+    sigma12 = F.conv2d(img1*img2, window, padding = window_size//2, groups = channel) - mu1_mu2
+
+    C1 = 0.01**2
+    C2 = 0.03**2
+
+    ssim_map = ((2*mu1_mu2 + C1)*(2*sigma12 + C2))/((mu1_sq + mu2_sq + C1)*(sigma1_sq + sigma2_sq + C2))
+
+    if size_average:
+        return ssim_map.mean()
+    else:
+        return ssim_map.mean(1).mean(1).mean(1)
+
+class SSIM(torch.nn.Module):
+    def __init__(self, window_size = 11, size_average = True):
+        super(SSIM, self).__init__()
+        self.window_size = window_size
+        self.size_average = size_average
+        self.channel = 1
+        self.window = create_window(window_size, self.channel)
+
+    def forward(self, img1, img2):
+        (_, channel, _, _) = img1.size()
+
+        if channel == self.channel and self.window.data.type() == img1.data.type():
+            window = self.window
+        else:
+            window = create_window(self.window_size, channel)
+
+            if img1.is_cuda:
+                window = window.cuda(img1.get_device())
+            window = window.type_as(img1)
+
+            self.window = window
+            self.channel = channel
+
+
+        return _ssim(img1, img2, window, self.window_size, channel, self.size_average)
+
+def _logssim(img1, img2, window, window_size, channel, size_average = True):
+    mu1 = F.conv2d(img1, window, padding = window_size//2, groups = channel)
+    mu2 = F.conv2d(img2, window, padding = window_size//2, groups = channel)
+
+    mu1_sq = mu1.pow(2)
+    mu2_sq = mu2.pow(2)
+    mu1_mu2 = mu1*mu2
+
+    sigma1_sq = F.conv2d(img1*img1, window, padding = window_size//2, groups = channel) - mu1_sq
+    sigma2_sq = F.conv2d(img2*img2, window, padding = window_size//2, groups = channel) - mu2_sq
+    sigma12 = F.conv2d(img1*img2, window, padding = window_size//2, groups = channel) - mu1_mu2
+
+    C1 = 0.01**2
+    C2 = 0.03**2
+
+    ssim_map = ((2*mu1_mu2 + C1)*(2*sigma12 + C2))/((mu1_sq + mu2_sq + C1)*(sigma1_sq + sigma2_sq + C2))
+    ssim_map = (ssim_map - torch.min(ssim_map))/(torch.max(ssim_map)-torch.min(ssim_map))
+    ssim_map = -torch.log(ssim_map + 1e-8)
+
+    if size_average:
+        return ssim_map.mean()
+    else:
+        return ssim_map.mean(1).mean(1).mean(1)
+
+class LOGSSIM(torch.nn.Module):
+    def __init__(self, window_size = 11, size_average = True):
+        super(LOGSSIM, self).__init__()
+        self.window_size = window_size
+        self.size_average = size_average
+        self.channel = 1
+        self.window = create_window(window_size, self.channel)
+
+    def forward(self, img1, img2):
+        (_, channel, _, _) = img1.size()
+
+        if channel == self.channel and self.window.data.type() == img1.data.type():
+            window = self.window
+        else:
+            window = create_window(self.window_size, channel)
+
+            if img1.is_cuda:
+                window = window.cuda(img1.get_device())
+            window = window.type_as(img1)
+
+            self.window = window
+            self.channel = channel
+
+
+        return _logssim(img1, img2, window, self.window_size, channel, self.size_average)
+
+
+def ssim(img1, img2, window_size = 11, size_average = True):
+    (_, channel, _, _) = img1.size()
+    window = create_window(window_size, channel)
+
+    if img1.is_cuda:
+        window = window.cuda(img1.get_device())
+    window = window.type_as(img1)
+
+    return _ssim(img1, img2, window, window_size, channel, size_average)
