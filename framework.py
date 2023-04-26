b6509ac74da3f8830b8774db642cf0c195da8734
lcy80366872 <106853511+lcy80366872@users.noreply.github.com>
2023-04-26 16:24:21 +0800

    Update framework.py

--- framework.py
+++ framework.py
@@ -1,13 +1,33 @@
+import matplotlib.pyplot as plt
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 from torch.autograd import Variable
+import cv2
 import os
 from tqdm import tqdm
 from utils.metrics import IoU
-from loss import dice_bce_loss
+from loss import dice_bce_loss,SSIM
 import copy
 import numpy
+from networks.DirectionNet import DirectionNet
+from networks.SGCN import Sobel
 
+
+import torch_dct as DCT
+
+def show_sobal(img,channels):
+    sobal=Sobel(channels, channels)
+    img_sobal=sobal(img)
+    print('img', img.shape)
+    print('sobla',img_sobal)
+    plt.subplot(1, 2, 1)
+    plt.imshow(img[0,0, :, :].cpu())
+    plt.subplot(1, 2, 2)
+    plt.imshow(img_sobal[0,:,:].cpu())
+    # plt.subplot(1, 3, 3)
+    # plt.imshow(b[0, 0, :, :].cpu())
+    plt.show()
 
 def L1_penalty(var):
     return torch.abs(var).sum()
@@ -15,12 +35,32 @@ class Solver:
     def __init__(self, net, optimizer, dataset):
         # self.net = torch.nn.DataParallel(net.cuda(), device_ids=list(range(torch.cuda.device_count())))
         self.net=net.cuda()
+        self.net_direction=DirectionNet().cuda()
         self.optimizer = optimizer
         self.dataset = dataset
-
-        self.loss = dice_bce_loss()
+        self.loss1 =dice_bce_loss(ssim=True)
+        self.loss = dice_bce_loss(ssim=False)
         self.metrics = IoU(threshold=0.5)
         self.old_lr = optimizer.param_groups[0]["lr"]
+    def resize(self, y_true, h, w):
+        b = y_true.shape[0]
+        y = numpy.zeros((b, h, w, y_true.shape[1]))
+        # print('y_t:', y_true.shape)
+        y_true = numpy.array(y_true.cpu())
+        for id in range(b):
+            y1 = y_true[id, :, :, :].transpose(1, 2, 0)
+            # print('y1:', y1.shape)
+            a = cv2.resize(y1, (h, w))
+
+            if a.ndim == 2:
+                a = numpy.expand_dims(a, axis=-1)
+            # print('a:', a.shape)
+            y[id, :, :, :] = a
+        # print(y.shape)
+        y = y.transpose(0, 3, 1,2)
+
+        return torch.Tensor(y)
+
 
     def set_input(self, img_batch, mask_batch=None):
         self.img = img_batch
@@ -87,7 +127,10 @@ class Solver:
         self.data2cuda()
 
         self.optimizer.zero_grad()
-        pred = self.net.forward(self.img)
+        # mask = self.resize(self.mask, 512, 512).cuda()
+        # direct_mask=self.net_direction.forward(mask)
+
+        pred1,pred = self.net.forward(self.img)
         slim_params = []
         for name, param in self.net.named_parameters():
             if param.requires_grad and name.endswith('weight') and 'bn2' in name:
@@ -96,10 +139,14 @@ class Solver:
                 else:
                     slim_params.append(param[len(param) // 2:])
 
-        loss = self.loss(self.mask,pred)
+        loss = self.loss(self.mask,pred1)
+        loss += self.loss1(self.mask, pred)
+        # loss += self.loss(self.mask, pred1)
+        # loss +=0.2*self.loss_direction(direct_pred,direct_mask)
         L1_norm = sum([L1_penalty(m).cuda() for m in slim_params])
         lamda =2e-4
         loss += lamda * L1_norm  # this is actually counted for len(outputs) times
+
 
         loss.backward()
         self.optimizer.step()
@@ -110,9 +157,12 @@ class Solver:
     def test_batch(self):
         self.net.eval()
         self.data2cuda(volatile=True)
-
-        pred = self.net.forward(self.img)
-        loss = self.loss(self.mask, pred)
+        # mask = self.resize(self.mask, 512, 512).cuda()
+        # direct_mask = self.net_direction.forward(mask)
+        pred1,pred = self.net.forward(self.img)
+        loss = self.loss(self.mask, pred1)
+        # loss += self.loss(self.mask, pred1)
+        # loss +=0.2*self.loss_direction(direct_pred,direct_mask)
 
         batch_iou, intersection, union = self.metrics(self.mask, pred)
         pred = pred.cpu().data.numpy().squeeze(1)
@@ -174,8 +224,8 @@ class Framework:
     def fit(self, epochs, no_optim_epochs=10):
         val_best_metrics = test_best_metrics = [0, 0]
         no_optim = 0
-#         scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer=self.solver.optimizer, T_max=epochs,
-#                                                                verbose=True)
+        scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer=self.solver.optimizer, T_max=30,
+                                                               verbose=True)
         for epoch in range(1, epochs + 1):
             print(f"epoch {epoch}/{epochs}")
 
@@ -190,7 +240,7 @@ class Framework:
                 no_optim = 0
             else:
                 no_optim += 1
-#             scheduler.step()
+            scheduler.step()
             if no_optim > no_optim_epochs:
                 if self.solver.old_lr < 1e-8:
                     print('early stop at {epoch} epoch')
@@ -265,3 +315,4 @@ class Framework:
         metrics = [average_iou, global_iou]
 
         return epoch_loss, metrics
+
