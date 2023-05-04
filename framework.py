cae8937e032581402bdd44a94bc0eb716c9ce0fd
lcy80366872 <106853511+lcy80366872@users.noreply.github.com>
2023-05-04 17:11:04 +0800

    Update framework.py

--- framework.py
+++ framework.py
@@ -152,6 +152,7 @@ class Solver:
                 a=param.detach()
 #                 b=list(filter(lambda x: x <0.02, a))
 #                 wandb.log({'bn': wandb.plot.line_series(a[0],a[1],a[2],a[3],a[4],a[5],a[6],a[7],a[8],a[9],a[10],a[11],a[12],a[13],a[14],a[15],a[16],a[17],a[18],a[19],a[20],a[21],a[22],a[23])})
+                a=list(np.array(a))
                 table=wandb.Table(data=a)
                 wandb.log({'bn': wandb.plot.line(table)})
                 break
@@ -178,6 +179,7 @@ class Solver:
             if param.requires_grad and name.endswith('weight') and 'bn2' in name:
                 a=param.detach()
 #                 b=list(filter(lambda x: x <0.02, a))
+                a=list(np.array(a))
                 table=wandb.Table(data=a)
                 wandb.log({'bn': wandb.plot.line(table)})
                 break
