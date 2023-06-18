f2f4b3eca64469bc1fd1368d436ce83ac3e11dea
lcy80366872 <106853511+lcy80366872@users.noreply.github.com>
2023-06-18 18:24:02 +0800

    Update basic_blocks.py

--- networks/basic_blocks.py
+++ networks/basic_blocks.py
@@ -42,9 +42,9 @@ class Exchange(nn.Module):
         bn1, bn2 = bn[0].weight.abs(), bn[1].weight.abs()
         x1, x2 = torch.zeros_like(x[0]), torch.zeros_like(x[1])
         x1[:, bn1 >= bn_threshold] = x[0][:, bn1 >= bn_threshold]
-        x1[:, bn1 < bn_threshold] = x[1][:, bn1 < bn_threshold]
+        x1[:, bn1 < bn_threshold] = x[0][:, bn1 < bn_threshold]+x[1][:, bn1 < bn_threshold]
         x2[:, bn2 >= bn_threshold] = x[1][:, bn2 >= bn_threshold]
-        x2[:, bn2 < bn_threshold] = x[0][:, bn2 < bn_threshold]
+        x2[:, bn2 < bn_threshold] = x[0][:, bn2 < bn_threshold]+x[1][:, bn2 < bn_threshold]
         return [x1, x2]
 class ModuleParallel(nn.Module):
     def __init__(self, module):
