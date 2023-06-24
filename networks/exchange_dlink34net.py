9fef9d416de63f2c94c82f335cbab5a1b27239cf
lcy80366872 <106853511+lcy80366872@users.noreply.github.com>
2023-06-24 11:02:40 +0800

    Update exchange_dlink34net.py

--- networks/exchange_dlink34net.py
+++ networks/exchange_dlink34net.py
@@ -160,9 +160,10 @@ class ResNet(nn.Module):
         # self.decoder3 = DecoderBlock_parallel_exchange(filters[2], filters[1],2,bn_threshold)
         # self.decoder2 = DecoderBlock_parallel_exchange(filters[1], filters[0],2,bn_threshold)
         # self.decoder1 = DecoderBlock_parallel_exchange(filters[0], filters[0],2,bn_threshold)
-        self.out_align = AlignModule(inplane=filters[0]//2)
-        # self.out_align2 = AlignModule(inplane=filters[1], outplane=filters[1])
-        # self.out_align3 = AlignModule(inplane=filters[2], outplane=filters[2])
+
+        self.out_align1 = AlignModule(inplane=filters[2])
+        self.out_align2 = AlignModule(inplane=filters[1])
+        self.out_align3 = AlignModule(inplane=filters[0])
 
 
         # self.finaldeconv1_add = nn.ConvTranspose2d(filters[0], filters[0] // 2, 4, 2, 1)
@@ -225,19 +226,25 @@ class ResNet(nn.Module):
 
         x_c = self.dblock(x_4)
         # decoder
-
-        x_d4 = [self.decoder4(x_c)[l] + x_3[l] for l in range(self.num_parallel)]
-        x_d3 = [self.decoder3(x_d4)[l] + x_2[l] for l in range(self.num_parallel)]
-        x_d2 = [self.decoder2(x_d3)[l] + x_1[l] for l in range(self.num_parallel)]
+        # x_d4 = [self.decoder4(x_c)[l] + x_3[l] for l in range(self.num_parallel)]
+        # x_d3 = [self.decoder3(x_d4)[l] + x_2[l] for l in range(self.num_parallel)]
+        # x_d2 = [self.decoder2(x_d3)[l] + x_1[l] for l in range(self.num_parallel)]
+        # x_d1 = self.decoder1(x_d2)
+        x_d4=  [self.out_align1(self.decoder4(x_c)[l],x_3[l]) for l in range(self.num_parallel)]
+        x_d4 = [x_d4[l] + x_3[l] for l in range(self.num_parallel)]
+        x_d3 = [self.out_align2(self.decoder3(x_d4)[l], x_2[l]) for l in range(self.num_parallel)]
+        x_d3 = [x_d3[l] + x_2[l] for l in range(self.num_parallel)]
+        x_d2 = [self.out_align3(self.decoder2(x_d3)[l], x_1[l]) for l in range(self.num_parallel)]
+        x_d2 = [x_d2[l] + x_1[l] for l in range(self.num_parallel)]
         x_d1 = self.decoder1(x_d2)
 
 
         x_out = self.finalrelu1(self.finaldeconv1(x_d1))
         x_out = self.finalrelu2(self.finalconv2(x_out))
 
-        # x_out[0]=self.se(x_out[0])
-        # x_out[1] = self.se(x_out[1])
-        x_out=self.out_align(x_out)
+        x_out[0]=self.se(x_out[0])
+        x_out[1] = self.se(x_out[1])
+        # x_out=self.out_align(x_out)
         # atten=self.atten(torch.cat((x_out[0], x_out[1]), 1))
         out = self.finalconv(torch.cat((x_out[0], x_out[1]), 1))
         # out=self.finalconv(x_out)
