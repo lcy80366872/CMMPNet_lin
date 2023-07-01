945e5ecec0dda1b413ec1240f22e9d996c390155
lcy80366872 <106853511+lcy80366872@users.noreply.github.com>
2023-07-01 10:13:09 +0800

    Update basic_blocks.py

--- networks/basic_blocks.py
+++ networks/basic_blocks.py
@@ -26,16 +26,16 @@ class Exchange(nn.Module):
     def forward(self, x, bn, bn_threshold):
         bn1, bn2 = bn[0].weight.abs(), bn[1].weight.abs()
         #就是大于阈值的那些通道保留，小于阈值的那些通道取另外一个的值
-        # x1, x2 = torch.zeros_like(x[0]), torch.zeros_like(x[1])
-        # x1[:, bn1 >= bn_threshold] = x[0][:, bn1 >= bn_threshold]
-        # x1[:, bn1 < bn_threshold] = x[1][:, bn1 < bn_threshold]
-        # x2[:, bn2 >= bn_threshold] = x[1][:, bn2 >= bn_threshold]
-        # x2[:, bn2 < bn_threshold] = x[0][:, bn2 < bn_threshold]
-        x[0][:, bn1 < bn_threshold] = x[1][:, bn1 < bn_threshold]
-        x[1][:, bn2 < bn_threshold] = x[0][:, bn2 < bn_threshold]
+        x1, x2 = torch.zeros_like(x[0]), torch.zeros_like(x[1])
+        x1[:, bn1 >= bn_threshold] = x[0][:, bn1 >= bn_threshold]
+        x1[:, bn1 < bn_threshold] = x[1][:, bn1 < bn_threshold]
+        x2[:, bn2 >= bn_threshold] = x[1][:, bn2 >= bn_threshold]
+        x2[:, bn2 < bn_threshold] = x[0][:, bn2 < bn_threshold]
+        # x[0][:, bn1 < bn_threshold] = x[1][:, bn1 < bn_threshold]
+        # x[1][:, bn2 < bn_threshold] = x[0][:, bn2 < bn_threshold]
         # print('bn',bn1 < bn_threshold)
 
-        return x
+        return x1,x2
 class SpatialAttention(nn.Module):
     def __init__(self, kernel_size=7):
         super().__init__()
