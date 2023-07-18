fb5af9adbeaa77cc511d1c0e13bc85f112823d39
lcy80366872 <106853511+lcy80366872@users.noreply.github.com>
2023-07-18 19:07:34 +0800

    Update exchange_dlink34net.py

--- networks/exchange_dlink34net.py
+++ networks/exchange_dlink34net.py
@@ -41,25 +41,25 @@ class BasicBlock(nn.Module):
 
     def forward(self, x):
         residual = x
-        mask_s_1, norm_s, norm_s_t = self.mask_s1(x[0])
-        mask_s_2, norm_s, norm_s_t = self.mask_s2(x[1])
+        # mask_s_1, norm_s, norm_s_t = self.mask_s1(x[0])
+        # mask_s_2, norm_s, norm_s_t = self.mask_s2(x[1])
 
         out = self.conv1(x)
 
         out = self.bn1(out)
         out = self.relu(out)
 
-        mask_s1 = self.upsample(mask_s_1)
-        mask_s2 = self.upsample(mask_s_2)
+        # mask_s1 = self.upsample(mask_s_1)
+        # mask_s2 = self.upsample(mask_s_2)
         # print(mask_s1)
-        out[0] = out[0] * mask_s1 + out[1] * (1 - mask_s1)
-        out[1] = out[1] * mask_s2 + out[0] * (1 - mask_s2)
+        # out[0] = out[0] * mask_s1 + out[1] * (1 - mask_s1)
+        # out[1] = out[1] * mask_s2 + out[0] * (1 - mask_s2)
 
         out = self.conv2(out)
         out = self.bn2(out)
 
-               # if len(x) > 1:
-        #     out = self.exchange(out, self.bn2_list, self.bn_threshold)
+        if len(x) > 1:
+            out = self.exchange(out, self.bn2_list, self.bn_threshold)
 
         if self.downsample is not None:
             residual = self.downsample(x)
