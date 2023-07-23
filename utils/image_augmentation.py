3a3bf49c077e713a6225beab7a7155133fb85f67
lcy80366872 <106853511+lcy80366872@users.noreply.github.com>
2023-07-23 17:45:40 +0800

    Update image_augmentation.py

--- utils/image_augmentation.py
+++ utils/image_augmentation.py
@@ -4,6 +4,10 @@ Based on https://github.com/asanakoy/kaggle_carvana_segmentation
 import cv2
 import numpy as np
 
+import random
+
+from PIL import Image, ImageEnhance
+
 
 def randomHueSaturationValue(
     image,
@@ -97,6 +101,62 @@ def randomVerticleFlip(image, mask, u=0.5):
         mask = cv2.flip(mask, 0)
 
     return image, mask
+
+def randomRotation(image, label, mode=Image.BICUBIC):
+    """
+     对图像进行随机任意角度(0~360度)旋转
+    :param mode 邻近插值,双线性插值,双三次B样条插值(default)
+    :param image PIL的图像image
+    :return: 旋转转之后的图像
+    """
+    random_angle = np.random.randint(1, 360)
+    return image.rotate(random_angle, mode), label.rotate(random_angle, Image.NEAREST)
+
+def randomColor(image, label):
+    """
+    对图像进行颜色抖动
+    :param image: PIL的图像image
+    :return: 有颜色色差的图像image
+    """
+    random_factor = np.random.randint(0, 31) / 10.  # 随机因子
+    color_image = ImageEnhance.Color(image).enhance(random_factor)  # 调整图像的饱和度
+    random_factor = np.random.randint(10, 21) / 10.  # 随机因子
+    brightness_image = ImageEnhance.Brightness(color_image).enhance(random_factor)  # 调整图像的亮度
+    random_factor = np.random.randint(10, 21) / 10.  # 随机因1子
+    contrast_image = ImageEnhance.Contrast(brightness_image).enhance(random_factor)  # 调整图像对比度
+    random_factor = np.random.randint(0, 31) / 10.  # 随机因子
+    return ImageEnhance.Sharpness(contrast_image).enhance(random_factor), label  # 调整图像锐度
+
+def randomGaussian(image, label, mean=0.2, sigma=0.3):
+    """
+     对图像进行高斯噪声处理
+    :param image:
+    :return:
+    """
+
+    def gaussianNoisy(im, mean=0.2, sigma=0.3):
+        """
+        对图像做高斯噪音处理
+        :param im: 单通道图像
+        :param mean: 偏移量
+        :param sigma: 标准差
+        :return:
+        """
+        for _i in range(len(im)):
+            im[_i] += random.gauss(mean, sigma)
+        return im
+
+    # 将图像转化成数组
+    img = np.asarray(image)
+    img.flags.writeable = True  # 将数组改为读写模式
+    width, height = img.shape[:2]
+    img_r = gaussianNoisy(img[:, :, 0].flatten(), mean, sigma)
+    img_g = gaussianNoisy(img[:, :, 1].flatten(), mean, sigma)
+    img_b = gaussianNoisy(img[:, :, 2].flatten(), mean, sigma)
+    img[:, :, 0] = img_r.reshape([width, height])
+    img[:, :, 1] = img_g.reshape([width, height])
+    img[:, :, 2] = img_b.reshape([width, height])
+    return Image.fromarray(np.uint8(img)), label
 
 
 def randomRotate90(image, mask, u=0.5):
