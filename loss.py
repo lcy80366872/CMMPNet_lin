111657e25c85531164454a0d78d4e6c729e33bce
lcy80366872 <106853511+lcy80366872@users.noreply.github.com>
2023-07-23 22:15:09 +0800

    Update loss.py

--- loss.py
+++ loss.py
@@ -120,18 +120,49 @@ class Loss_atten(nn.Module):
         unfold_y_gt_conv = self.unfold(y_gt_conv)
         att=torch.matmul(unfold_y_gt,unfold_y_gt_conv)/(self.patch_size*self.patch_size)
         att=torch.unsqueeze(att,dim=1)
-
+        print(att[0][0])
+        print(att.shape)
         loss = self.mseloss(attentions, att)
 
 
         return loss
 
+from scipy.ndimage.morphology import distance_transform_edt
+def mask_to_onehot(mask, num_classes):
+    """
+    Converts a segmentation mask (H,W) to (K,H,W) where the last dim is a one
+    hot encoding vector
+
+    """
+    _mask = [mask == (i) for i in range(num_classes)]
+    return np.array(_mask).astype(np.uint8)
+
+def onehot_to_binary_edges(mask, radius, num_classes):
+    """
+    Converts a segmentation mask (K,H,W) to a binary edgemap (H,W)
+    """
+    if radius < 0:
+        return mask
+
+    # We need to pad the borders for boundary conditions
+    mask_pad = np.pad(mask, ((0, 0), (1, 1), (1, 1)), mode='constant', constant_values=0)
+
+    edgemap = np.zeros(mask.shape[1:])
+    for i in range(num_classes):
+        # ti qu lun kuo
+        dist = distance_transform_edt(mask_pad[i, :]) + distance_transform_edt(1.0 - mask_pad[i, :])
+        dist = dist[1:-1, 1:-1]
+        dist[dist > radius] = 0
+        edgemap += dist
+    # edgemap = np.expand_dims(edgemap, axis=0)
+    edgemap = (edgemap > 0).astype(np.uint8)*255
+    return edgemap
 
 class dice_bce_loss(nn.Module):
     def __init__(self,ssim=False, batch=True):
         super(dice_bce_loss, self).__init__()
         self.batch = batch
-        self.bce_loss = nn.BCELoss()
+        self.bce_loss = nn.BCELoss(reduction='none')
         self.focal_loss = BinaryFocalLoss(gamma=2,alpha=0.75)
         self.ifssim=ssim
         self.ssim=MS_SSIM_L1_LOSS()
@@ -190,7 +221,7 @@ class dice_bce_loss(nn.Module):
 
         return torch.Tensor(y)
 
-    def __call__(self, y_true, y_pred):
+    def __call__(self, y_true, y_pred,atten):
         # the ground_truth map is resized to the resolution of the predicted map during training
         if y_true.shape[2] != y_pred.shape[2] or y_true.shape[3] != y_pred.shape[3]:
             y_true = self.resize(y_true, y_pred.shape[2], y_pred.shape[3]).cuda()
@@ -199,15 +230,27 @@ class dice_bce_loss(nn.Module):
         # a = self.focal_loss(y_pred,y_true)
         #
         a = self.bce_loss(y_pred, y_true)
-        # print('bce', a)
+        oneHot_label = mask_to_onehot(y_true, 2)
+        edge = onehot_to_binary_edges(oneHot_label, 2, 2)  # #edge=255,background=0
+        edge[:2, :] = 0
+        edge[-2:, :] = 0
+        edge[:, :2] = 0
+        edge[:, -2:] = 0
+        edge_weight=4
+        edge[edge == 0] = 1.
+        edge[edge == 255] = edge_weight
+        a *= edge
+        loss_bce, ind = a.contiguous().view(-1).sort().mean()
+
         b = self.soft_dice_loss(y_true, y_pred)
         # d= self.attloss(y_pred,y_true,atten)
-        # print(d)
+        # print('11',a+b)
+        # print('atten',d)
         if self.ifssim:
             c =  self.ssim(y_pred,y_true )
             return a+b+c
         else:
-            return a + b 
+            return loss_bce + b
 #
 #
 #
