1c8e2f656d867c753fdae8d75dad002e649906cc
lcy80366872 <106853511+lcy80366872@users.noreply.github.com>
2023-07-23 20:28:07 +0800

    Update data_loader.py

--- utils/data_loader.py
+++ utils/data_loader.py
@@ -160,7 +160,7 @@ class ImageGPSDataset(data.Dataset):
             img, mask = randomRotate90(img, mask)
             img, mask = randomcrop(img, mask)
             # img, mask = randomColor(img,mask)
-            img, mask = randomGaussian(img,mask)
+            # img, mask = randomGaussian(img,mask)
         else:
             img = self._concat_images(sat, gps)
         #             ycb = cv2.cvtColor(sat, cv2.COLOR_BGR2YCrCb)
