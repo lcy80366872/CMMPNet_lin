5dfd3fecdf631fcea83a866998cb25ef3ab8a2da
lcy80366872 <106853511+lcy80366872@users.noreply.github.com>
2023-07-24 10:43:55 +0800

    Update loss.py

--- loss.py
+++ loss.py
@@ -224,7 +224,7 @@ class dice_bce_loss(nn.Module):
 
         return torch.Tensor(y)
 
-    def __call__(self, y_true, y_pred,atten):
+    def __call__(self, y_true, y_pred):
         # the ground_truth map is resized to the resolution of the predicted map during training
         if y_true.shape[2] != y_pred.shape[2] or y_true.shape[3] != y_pred.shape[3]:
             y_true = self.resize(y_true, y_pred.shape[2], y_pred.shape[3]).cuda()
