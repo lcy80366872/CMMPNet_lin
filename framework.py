cd33f452ab29caab7ce454a478e200d4d746e716
lcy80366872 <106853511+lcy80366872@users.noreply.github.com>
2023-09-10 11:29:35 +0800

    Update framework.py

--- framework.py
+++ framework.py
@@ -1,80 +1,26 @@
-import matplotlib.pyplot as plt
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
 from torch.autograd import Variable
-import cv2
 import os
 from tqdm import tqdm
 from utils.metrics import IoU
 from loss import dice_bce_loss
 import copy
 import numpy
-from networks.DirectionNet import DirectionNet
-from networks.SGCN import Sobel
 
-
-
-import torch_dct as DCT
-
-def show_sobal(img,channels):
-    sobal=Sobel(channels, channels)
-    img_sobal=sobal(img)
-    print('img', img.shape)
-    print('sobla',img_sobal)
-    plt.subplot(1, 2, 1)
-    plt.imshow(img[0,0, :, :].cpu())
-    plt.subplot(1, 2, 2)
-    plt.imshow(img_sobal[0,:,:].cpu())
-    # plt.subplot(1, 3, 3)
-    # plt.imshow(b[0, 0, :, :].cpu())
-    plt.show()
 
 def L1_penalty(var):
     return torch.abs(var).sum()
-
-
-def _compute_polarization_sparsity(sparse_modules: list, lbd, t, alpha, bn_weights_mean):
-    sparsity_loss = 0
-    n=0
-    for m in sparse_modules:
-        sparsity_term = t * torch.sum(torch.abs(m)) - torch.sum(
-                torch.abs(m - alpha * bn_weights_mean[n]))
-        sparsity_loss += lbd * sparsity_term
-        n=n+1
-
-    return sparsity_loss
-
 class Solver:
     def __init__(self, net, optimizer, dataset):
         # self.net = torch.nn.DataParallel(net.cuda(), device_ids=list(range(torch.cuda.device_count())))
         self.net=net.cuda()
-        self.net_direction=DirectionNet().cuda()
         self.optimizer = optimizer
         self.dataset = dataset
-        self.loss1 =dice_bce_loss(ssim=True)
-        self.loss = dice_bce_loss(ssim=False)
-        self.metrics = IoU(threshold=0.3)
+
+        self.loss = dice_bce_loss()
+        self.metrics = IoU(threshold=0.5)
         self.old_lr = optimizer.param_groups[0]["lr"]
-    def resize(self, y_true, h, w):
-        b = y_true.shape[0]
-        y = numpy.zeros((b, h, w, y_true.shape[1]))
-        # print('y_t:', y_true.shape)
-        y_true = numpy.array(y_true.cpu())
-        for id in range(b):
-            y1 = y_true[id, :, :, :].transpose(1, 2, 0)
-            # print('y1:', y1.shape)
-            a = cv2.resize(y1, (h, w))
-
-            if a.ndim == 2:
-                a = numpy.expand_dims(a, axis=-1)
-            # print('a:', a.shape)
-            y[id, :, :, :] = a
-        # print(y.shape)
-        y = y.transpose(0, 3, 1,2)
-
-        return torch.Tensor(y)
-
 
     def set_input(self, img_batch, mask_batch=None):
         self.img = img_batch
@@ -131,7 +77,7 @@ class Solver:
     #         loss += lamda * L1_norm  # this is actually counted for len(outputs) times
     #
     #     loss.backward()
-        # self.optimizer.step()
+    #     self.optimizer.step()
     #
     #     batch_iou, intersection, union = self.metrics(self.mask, outs[0])
     #     return outs[0], loss.item(), batch_iou, intersection, union
@@ -141,55 +87,35 @@ class Solver:
         self.data2cuda()
 
         self.optimizer.zero_grad()
-        # mask = self.resize(self.mask, 512, 512).cuda()
-        # direct_mask=self.net_direction.forward(mask)
-
         pred = self.net.forward(self.img)
         slim_params = []
-        mean_params=[]
-        sparse=0
         for name, param in self.net.named_parameters():
-            if param.requires_grad and name.endswith('weight') and 'bn2' in name :
+            if param.requires_grad and name.endswith('weight') and 'bn2' in name:
                 if len(slim_params) % 2 == 0:
                     slim_params.append(param[:len(param) // 2])
-                #     mean_params.append(torch.mean(param[:len(param) // 2]))
                 else:
                     slim_params.append(param[len(param) // 2:])
-                #     mean_params.append(torch.mean(param[len(param) // 2:]))
-                # slim_params.append(param)
-                # mean_params.append(torch.mean(param))
 
-                # sparse_weights_mean=param.mean()
-                # print(sparse_weights_mean)
-        # sparse =_compute_polarization_sparsity(slim_params,lbd=2e-4, t=1.2,alpha=1,bn_weights_mean=mean_params)
-        # print(sparse)
         loss = self.loss(self.mask,pred)
-        # loss +=sparse
+        # loss += self.loss(self.mask,aux)
         L1_norm = sum([L1_penalty(m).cuda() for m in slim_params])
         lamda =2e-4
         # loss += lamda * L1_norm  # this is actually counted for len(outputs) times
-
-
-
+        # print('l1',lamda * L1_norm)
         loss.backward()
         self.optimizer.step()
 
         batch_iou, intersection, union = self.metrics(self.mask, pred)
-       
         return pred, loss.item(), batch_iou, intersection, union
 
     def test_batch(self):
         self.net.eval()
         self.data2cuda(volatile=True)
-        # mask = self.resize(self.mask, 512, 512).cuda()
-        # direct_mask = self.net_direction.forward(mask)
-        pred = self.net.forward(self.img)
+
+        pred= self.net.forward(self.img)
         loss = self.loss(self.mask, pred)
-        # loss += self.loss(self.mask, pred1)
-        # loss +=0.2*self.loss_direction(direct_pred,direct_mask)
 
         batch_iou, intersection, union = self.metrics(self.mask, pred)
-       
         pred = pred.cpu().data.numpy().squeeze(1)
         return pred, loss.item(), batch_iou, intersection, union
     def test_batch_exchange(self):
@@ -246,10 +172,10 @@ class Framework:
     def set_save_path(self, save_path):
         self.save_path = save_path
 
-    def fit(self, epochs, no_optim_epochs=5):
+    def fit(self, epochs, no_optim_epochs=4):
         val_best_metrics = test_best_metrics = [0, 0]
         no_optim = 0
-        # scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer=self.solver.optimizer, T_max= epochs,
+        # scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer=self.solver.optimizer, T_max=epochs,
         #                                                        verbose=True)
         for epoch in range(1, epochs + 1):
             print(f"epoch {epoch}/{epochs}")
@@ -273,7 +199,7 @@ class Framework:
                 else:
                     no_optim = 0
                     self.solver.update_lr(ratio=5.0)
-            
+
             print(f'train_loss: {train_loss:.4f} train_metrics: {train_metrics}')
             print(f'  val_loss: {val_loss:.4f}   val_metrics:   {val_metrics}')
             print(f' test_loss: {test_loss:.4f}  test_metrics:  {test_metrics}')
@@ -340,3 +266,4 @@ class Framework:
         metrics = [average_iou, global_iou]
 
         return epoch_loss, metrics
+
