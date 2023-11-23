a2a39649e9b7df9ced1c6c956fe0746533e176b6
lcy80366872 <106853511+lcy80366872@users.noreply.github.com>
2023-11-23 17:00:01 +0800

    Update main.py

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ from framework import Framework
 from utils.datasets import prepare_Beijing_dataset, prepare_TLCGIS_dataset
 from networks.exchange_dlink34net import DinkNet34_CMMPNet
 from networks.deeplabv3plus import DeepLabV3Plus
-from networks.unet import unet
+from networks.unet import Unet
 
 class Logger(object):
     def __init__(self, filename="Default.log"):
