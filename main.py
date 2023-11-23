e86eb2feb28dcde037ccf2f79b228f0b105ba702
lcy80366872 <106853511+lcy80366872@users.noreply.github.com>
2023-11-23 16:03:24 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,22 +1,20 @@
 import torch
 import numpy as np
-# from torchsummary import summary
+from torchsummary import summary
 import time
 import torch.nn as nn
 from torch.nn import init
 import os
 import sys
-from torch_lr_finder import LRFinder
 import cv2
-from loss import dice_bce_loss
 from utils.model_init import model_init
 from framework import Framework
-import wandb
 # from framework_connect import Framework
 # from utils.datasets_connect import prepare_Beijing_dataset, prepare_TLCGIS_dataset
 from utils.datasets import prepare_Beijing_dataset, prepare_TLCGIS_dataset
-from networks.dlinknet34 import DinkNet34_CMMPNet
-os.environ['CUDA_LAUNCH_BLOCKING'] = '1'
+from networks.exchange_dlink34net import DinkNet34_CMMPNet
+from networks.deeplabv3plus import DeepLabV3Plus
+
 class Logger(object):
     def __init__(self, filename="Default.log"):
         self.terminal = sys.stdout
@@ -31,7 +29,9 @@ class Logger(object):
 
 def get_model(model_name):
     if model_name == 'CMMPNet':
-       model = DinkNet34_CMMPNet()
+        model = DeepLabV3Plus(
+            n_classes=1, num_channels=4
+        )
     else:
         print("[ERROR] can not find model ", model_name)
         assert(False)
@@ -79,18 +79,19 @@ def pre_image(net):
 
 def train_val_test(args):
     net = get_model(args.model)
-#     with torch.no_grad():  # 蹇呴』鏈�
+#     with torch.no_grad():  # 必须有
 #         summary(net.to('cuda'), input_size=(4, 512, 512), batch_size=4)
-    model_init(net, 'resnet34', 2, imagenet=True)
+#    model_init(net, 'resnet34', 2, imagenet=True)
 #     print(net)
     print('lr:',args.lr)
     optimizer = torch.optim.Adam(params=net.parameters(), lr=args.lr)
-    # 澶歡pu寰楀埌鐨勬ā鍨媎ict鍓嶉潰浼氬姞module
+    # 多gpu得到的模型dict前面会加module
 #     new_state = {} 
-#     state_dict = torch.load('/kaggle/input/connect-v2-model/connect_v2_epoch19_val0.6588_test0.6342.pth', map_location=torch.device('cpu'))
+#    state_dict = torch.load('save_model/edge_val0.6420_test0.6098.pth')#, map_location=torch.device('cpu'))
 #     for key, value in state_dict.items():
 #         new_state[key.replace('module.', '')] = value
 #     net.load_state_dict(new_state)
+#     net.load_state_dict(state_dict)
     
     
 #     pre_image(net)
@@ -98,45 +99,12 @@ def train_val_test(args):
     framework = Framework(net, optimizer, dataset=args.dataset)
     
     train_dl, val_dl, test_dl = get_dataloader(args)
-    
-#     criterion = dice_bce_loss()
-#     lr_finder = LRFinder(net, optimizer, criterion, device="cuda")
-#     lr_finder.range_test(train_dl,val_loader=val_dl, end_lr=1, num_iter=100,step_mode="linear")
-#     lr_finder.plot()  # to inspect the loss-learning rate graph
-#     lr_finder.reset()  # to reset the model and optimizer to their initial state
-
     framework.set_train_dl(train_dl)
     framework.set_validation_dl(val_dl)
     framework.set_test_dl(test_dl)
     framework.set_save_path(WEIGHT_SAVE_DIR)
-#     wandb.init(
-#     # set the wandb project where this run will be logged
-#     project="exchange_bnthrold",
-#     config={
-#     "lr": args.lr,
-#     "model": args.model,
-#     "batch_size": args.batch_size,
-#     "gpu_ids":  args.gpu_ids ,
-#     "epochs":  args.epochs,
-#     "dataset": args.dataset
-#         })
-#     sweep_configuration = {
-#         'method': 'random',
-#         'name': 'sweep',
-#         'metric': {
-#             'goal': 'maximize',
-#             'name': 'test_metrics'
-#         },
-#         'parameters': {
-#             'batch_size': {'values': [2, 4, 8]},
-#             'lr': {'max': 0.1, 'min': 0.0001}
-#         }
-#     }
 
-#     sweep_id = wandb.sweep(sweep=sweep_configuration, project="exchange_search")
-#     wandb.agent(sweep_id, function=framework.fit, count=8)
-
-    framework.fit(epochs=args.epochs)
+    framework.fit(epochs=args.epochs,lam=2e-4)
 
 
 if __name__ == "__main__":
@@ -146,25 +114,34 @@ if __name__ == "__main__":
     parser.add_argument('--model', type=str, default='CMMPNet')
     parser.add_argument('--lr',    type=float, default=2e-4)
     parser.add_argument('--name',  type=str, default='')
-    parser.add_argument('--batch_size', type=int, default=4)
-    parser.add_argument('--sat_dir',  type=str, default=r'F:\ML_data\remote_data\BJRoad\train_val\image')
-    parser.add_argument('--mask_dir', type=str, default=r'F:\ML_data\remote_data\BJRoad\train_val\mask')
-    parser.add_argument('--gps_dir',  type=str, default=r'F:\ML_data\remote_data\BJRoad\train_val\gps')
-    parser.add_argument('--test_sat_dir',  type=str, default=r'F:\ML_data\remote_data\BJRoad\test\image')
-    parser.add_argument('--test_mask_dir', type=str, default=r'F:\ML_data\remote_data\BJRoad\test\mask')
-    parser.add_argument('--test_gps_dir',  type=str, default=r'F:\ML_data\remote_data\BJRoad\test\gps')
+    parser.add_argument('--batch_size', type=int, default=1)
+    # parser.add_argument('--sat_dir', type=str, default='/home/imi432004/porto_dataset/rgb')
+    # parser.add_argument('--mask_dir', type=str, default='/home/imi432004/porto_dataset/mask')
+    # parser.add_argument('--gps_dir', type=str, default='/home/imi432004/porto_dataset/gps')
+    # parser.add_argument('--edge_dir', type=str, default='/home/imi432004/BJRoad/BJRoad/train_val/edge')
+    # parser.add_argument('--test_sat_dir', type=str, default='/home/imi432004/BJRoad/BJRoad/test/image')
+    # parser.add_argument('--test_mask_dir', type=str, default='/home/imi432004/BJRoad/BJRoad/test/mask')
+    # parser.add_argument('--test_gps_dir', type=str, default='/home/imi432004/BJRoad/BJRoad/test/gps')
+    parser.add_argument('--sat_dir',  type=str, default=r'E:\ML_data\remote_data\BJRoad\train_val\image')
+    parser.add_argument('--mask_dir', type=str, default=r'E:\ML_data\remote_data\BJRoad\train_val\mask')
+    parser.add_argument('--gps_dir',  type=str, default=r'E:\ML_data\remote_data\BJRoad\train_val\gps')
+    parser.add_argument('--edge_dir', type=str, default=r'E:\ML_data\remote_data\BJRoad\train_val\edge')
+    parser.add_argument('--test_edge_dir', type=str, default=r'E:\ML_data\remote_data\BJRoad\test\edge')
+    parser.add_argument('--test_sat_dir',  type=str, default=r'E:\ML_data\remote_data\BJRoad\test\image')
+    parser.add_argument('--test_mask_dir', type=str, default=r'E:\ML_data\remote_data\BJRoad\test\mask')
+    parser.add_argument('--test_gps_dir',  type=str, default=r'E:\ML_data\remote_data\BJRoad\test\gps')
     parser.add_argument('--connect_dir1',  type=str, default=r'F:\ML_data\remote_data\BJRoad\train_val\connect_8_d1')
     parser.add_argument('--connect_dir2', type=str, default=r'F:\ML_data\remote_data\BJRoad\train_val\connect_8_d3')
     parser.add_argument('--test_connect_dir1', type=str,default=r'F:\ML_data\remote_data\BJRoad\test\connect_8_d1')
     parser.add_argument('--test_connect_dir2', type=str,default=r'F:\ML_data\remote_data\BJRoad\test\connect_8_d3')
-    parser.add_argument('--lidar_dir',  type=str, default='')
-    parser.add_argument('--split_train_val_test', type=str, default='')
+    parser.add_argument('--lidar_dir',  type=str, default='/home/imi432004/porto_dataset/gps')
+    parser.add_argument('--split_train_val_test', type=str, default='/home/imi432004/porto_dataset/split_5')
     parser.add_argument('--weight_save_dir', type=str, default='./save_model')
     parser.add_argument('--val_size', type=float, default=0.1)
     parser.add_argument('--use_gpu',  type=bool, default=True)
     parser.add_argument('--gpu_ids',  type=str, default='0')
     parser.add_argument('--workers',  type=int, default=0)
-    parser.add_argument('--epochs',  type=int, default=1)
+    parser.add_argument('--epochs',  type=int, default=30)
     parser.add_argument('--random_seed', type=int, default=12345)
     parser.add_argument('--dataset', type=str, default='BJRoad')
     parser.add_argument('--down_scale', type=bool, default=False)
@@ -188,7 +165,8 @@ if __name__ == "__main__":
     path = os.path.abspath(os.path.dirname(__file__))
     type = sys.getfilesystemencoding()
     sys.stdout = Logger(WEIGHT_SAVE_DIR+'train.log')
-    
-
+    torch.manual_seed(114514)
+    torch.cuda.manual_seed(114514)
     train_val_test(args)
     print("[DONE] finished")
+
